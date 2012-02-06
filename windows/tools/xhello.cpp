6a23991b28bc361f22df801e9dec962479bc2736
Frank Mertens <frank@cyblogic.de>
2012-02-06 22:01:29 +0100

    Growing X11 protocol support.
    Signed-off-by: Frank Mertens <frank@cyblogic.de>

--- windows/tools/xhello.cpp
+++ windows/tools/xhello.cpp
@@ -7,6 +7,7 @@ int main()
 	using namespace ftl;
 	Ref<XWindow, Owner> window = xClient()->createWindow(100, 100, 200, 200);
 	xClient()->mapWindow(window);
-	while (true) Process::sleep(1);
+	xClient()->start();
+	xClient()->wait();
 	return 0;
 }
