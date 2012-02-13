6d9fc3be00dcf3f856beef6762d2ddfeeb6a8f23
Frank Mertens <frank@cyblogic.de>
2012-02-13 15:40:06 +0100

    Split up of the Channel class.
    Fixed length message queues have proven inappropriate in modelling
    multithreaded systems. Fixed length message queues can block on both
    ends and therefore increase chances of a deadlock or require an
    inappropriate amount of memory.
    Therefore an unlimited length queue was introduced (Queue) and the
    old Queue class was renamed into CircularBuffer. The Channel class
    has beend spezialised to an unlimited length message queue. The
    other concepts have been moved to the Rondezvous and Conveyor classes.
    
    Signed-off-by: Frank Mertens <frank@cyblogic.de>

---
+++ system/ftl/Rondezvous.hpp
@@ -0,0 +1,78 @@
+/*
+ * Rondezvous.hpp -- passing data between threads with handshake
+ *
+ * Copyright (c) 2007-2012, Frank Mertens
+ *
+ * This file is part of the a free software library. You can redistribute
+ * it and/or modify it under the terms of FTL's 2-clause BSD license.
+ *
+ * See the LICENSE.txt file for details at the top-level of FTL's sources.
+ */
+#ifndef FTL_RONDEZVOUS_HPP
+#define FTL_RONDEZVOUS_HPP
+
+#include "containers.hpp"
+#include "Mutex.hpp"
+#include "Condition.hpp"
+
+namespace ftl
+{
+
+template<class T>
+class Rondezvous: public Container< T, Rondezvous<T> >
+{
+public:
+	Rondezvous(): isFull_(false) {}
+
+	Rondezvous& push(const T& item)
+	{
+		tableMutex_.acquire();
+		while (isFull_)
+			emptyCondition_.wait(&tableMutex_);
+		table_ = item;
+		isFull_ = true;
+		ackMutex_.acquire();
+		ack_.signal();
+		fullCondition_.signal();
+		tableMutex_.release();
+		ack_.wait(&ackMutex_);
+		ackMutex_.release();
+		return *this;
+	}
+
+	Rondezvous& pop(T* item)
+	{
+		tableMutex_.acquire();
+		while (!isFull_)
+			fullCondition_.wait(&tableMutex_);
+		ackMutex_.acquire();
+		ack_.signal();
+		ackMutex_.release();
+		*item = table_;
+		isFull_ = false;
+		emptyCondition_.signal();
+		tableMutex_.release();
+		return *this;
+	}
+
+	inline T pop() {
+		T item;
+		pop(&item);
+		return item;
+	}
+
+	inline bool isEmpty() const { return false; }
+
+private:
+	T table_;
+	Mutex tableMutex_;
+	bool isFull_;
+	Condition emptyCondition_;
+	Condition fullCondition_;
+	Mutex ackMutex_;
+	Condition ack_;
+};
+
+} // namespace ftl
+
+#endif // FTL_RONDEZVOUS_HPP
--- system/ftl/containers.hpp
+++ system/ftl/containers.hpp
@@ -25,7 +25,7 @@ public:
 	
 	inline bool read(T* item) {
 		if (!isEmpty()) {
-			*item = pop();
+			pop(item);
 			return true;
 		}
 		return false;
