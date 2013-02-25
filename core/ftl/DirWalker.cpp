8de212bc1b5b6ecbda44aa852669ffbb28acd167
Frank Mertens <frank@cyblogic.de>
2013-02-25 18:05:34 +0100

    Improvements for the DirWalker.
    Signed-off-by: Frank Mertens <frank@cyblogic.de>

--- core/ftl/DirWalker.cpp
+++ core/ftl/DirWalker.cpp
@@ -6,28 +6,30 @@
   * as published by the Free Software Foundation; either version
   * 2 of the License, or (at your option) any later version.
   */
+
 #include "Dir.hpp"
-#include "FileStatus.hpp"
+#include "File.hpp"
 #include "DirWalker.hpp"
 
 namespace ftl
 {
 
-Ref<DirWalker> DirWalker::tryOpen(String path, int option)
+Ref<DirWalker> DirWalker::tryOpen(String path)
 {
 	Ref<Dir> dir = Dir::tryOpen(path);
-	if (dir) return new DirWalker(path, option, dir);
+	if (dir) return new DirWalker(path, dir);
 	return 0;
 }
 
-Ref<DirWalker> DirWalker::open(String path, int option)
+Ref<DirWalker> DirWalker::open(String path)
 {
-	return new DirWalker(path, option);
+	return new DirWalker(path);
 }
 
-DirWalker::DirWalker(String path, int option, Dir *dir)
-	: option_(option),
-	  maxDepth_(-1),
+DirWalker::DirWalker(String path, Dir *dir)
+	: maxDepth_(-1),
+	  followSymlink_(false),
+	  deleteOrder_(false),
 	  depth_(0),
 	  dir_(dir)
 {
@@ -39,8 +41,7 @@ bool DirWalker::read(String *path)
 	if (child_) {
 		if (child_->read(path))
 			return true;
-		--depth_;
-		if ((option_ != FilesOnly) && (option_ != DirsFirst)) {
+		if (deleteOrder_) {
 			*path = child_->dir_->path();
 			child_ = 0;
 			return true;
@@ -49,14 +50,26 @@ bool DirWalker::read(String *path)
 	}
 	String name;
 	while (dir_->read(&name)) {
-		if ((name == "") || (name == ".") || (name == "..")) continue;
+		if (name == "" || name == "." || name == "..") continue;
 		String h = dir_->path(name);
-		child_ = tryOpen(h, option_);
+		child_ = tryOpen(h);
 		if (child_) {
-			if (depth_ == maxDepth_) continue;
-			++depth_;
-			if ((option_ != FilesOnly) && (option_ != DirsFirst))
-				return read(path);
+			if (depth_ != maxDepth_) {
+				if (!followSymlink_ && File::readlink(h) != "") {
+					child_ = 0;
+				}
+				else {
+					child_->maxDepth_ = maxDepth_;
+					child_->followSymlink_ = followSymlink_;
+					child_->deleteOrder_ = deleteOrder_;
+					child_->depth_ = depth_ + 1;
+					if (deleteOrder_)
+						return read(path);
+				}
+			}
+			else {
+				child_ = 0;
+			}
 		}
 		*path = h;
 		return true;
