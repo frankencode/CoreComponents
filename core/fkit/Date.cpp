0eb8d2c9ebc12b41bee4b9859aa6e6a2b24f4ecc
Frank Mertens <frank@cyblogic.de>
2013-09-12 22:43:49 +0200

    Scrunitized calendar algorithms to fully support the range of year 1 to year 9999
    Signed-off-by: Frank Mertens <frank@cyblogic.de>

--- core/fkit/Date.cpp
+++ core/fkit/Date.cpp
@@ -8,6 +8,7 @@
  */
 
 #include <stdint.h>
+#include <time.h>
 #include "System.h"
 #include "Format.h"
 #include "Date.h"
@@ -23,11 +24,18 @@ inline bool leapYear(int y)
 	return (y % 4 == 0) && ((y % 100 != 0) || (y % 400 == 0));
 }
 
-/** Minor improvement of the leapYear definition proposed by Sir John Herschel.
+/** Number of leap years between x and y (excluding x, y).
   */
-inline bool herschelLeapYear(int y)
+int leaps(int x, int y)
 {
-	return leapYear(y) && (y % 4000 != 0);
+	if (x > y) {
+		int h = x;
+		x = y;
+		y = h;
+	}
+	--y; ++x; // exclude year x, y
+	if (x > y) return 0;
+	return y / 4 - y / 100 + y / 400 - (x - 1) / 4 + (x - 1) / 100 - (x - 1) / 400;
 }
 
 /** Days in a year are arbitrarily distributed but weighted to make the
@@ -49,8 +57,115 @@ inline int daysInMonth(int i, int y)
 	return days[i] + (i == 1) * leapYear(y);
 }
 
-Date::Date(): tm_off(-2), time_(fkit::nan) { clear(); }
-Date::Date(double time): tm_off(0), time_(time) { clear(); init(time); }
+Date::Date()
+	: tm_off(0),
+	  time_(fkit::nan)
+{
+	tm_isdst = -1;
+}
+
+Date::Date(double time)
+	: tm_off(0),
+	  time_(time)
+{
+	const int C1   = 365;
+	const int C4   =  4 * C1 + 1;
+	const int C100 = 25 * C4 - 1;
+	const int C400 =  4 * C100 + 1;
+	const int N0   = 719528; // linear day number of Jan 1st year 0
+
+	int64_t t = int64_t(time) + int64_t(N0) * 86400;
+	int n = t / 86400;
+	if (n < 0) n = 0;
+
+	tm_hour = (t / 3600) % 24;
+	tm_min = (t / 60) % 60;
+	tm_sec = t % 60;
+	tm_wday = (n + 6) % 7;
+
+	int y = 400 * (n / C400);
+	n = n % C400;
+
+	while (true) {
+		int h = C100 + leapYear(y);
+		if (n < h) break;
+		n -= h;
+		y += 100;
+	}
+
+	while (true) {
+		int h = C4 - !leapYear(y);
+		if (n < h) break;
+		n -= h;
+		y += 4;
+	}
+
+	while (true) {
+		int h = C1 + leapYear(y);
+		if (n < h) break;
+		n -= h;
+		++y;
+	}
+
+	tm_year = y - 1900;
+	tm_yday = n;
+
+	tm_mon = 0;
+	while (true) {
+		int h = daysInMonth(tm_mon, y);
+		if (n < h) break;
+		++tm_mon;
+		n -= h;
+	}
+	tm_mday = n + 1;
+
+	tm_isdst = -1;
+}
+
+Date::Date(int year, int month, int day, int hour, int seconds, int minutes, int offset)
+{
+	if (year < 1) year = 1;
+	if (month > 12) month = 12;
+	else if (month < 1) month = 1;
+	if (day > 31) day = 31;
+	else if (day < 1) day = 1;
+	if (hour < 0) hour = 0;
+	else if (hour > 23) hour = 23;
+	if (minutes > 59) minutes = 59;
+	else if (minutes < 0) minutes = 0;
+	if (seconds > 59) seconds = 59;
+	else if (seconds < 0) seconds = 0;
+	if (offset > 600) offset = 600;
+	else if (offset < -600) offset = -600;
+
+	--day;
+	--month;
+
+	int64_t t = 0;
+	for (int i = 0; i < month; ++i)
+		t += daysInMonth(i, year);
+	t += day;
+	tm_yday = t;
+	t += (31 * 7 + 30 * 4 + 28) * (year - 1970);
+	if (year >= 1970)
+		t += leaps(1970, year);
+	else
+		t -= leaps(1970, year - 1);
+	tm_wday = (719528 + t + 6) % 7;
+	t *= 86400;
+	t += 3600 * hour + 60 * minutes + seconds;
+	time_ = t;
+
+	tm_mday = day + 1;
+	tm_mon = month;
+	tm_year = year - 1900;
+	tm_hour = hour;
+	tm_min = minutes;
+	tm_sec = seconds;
+	tm_off = offset;
+
+	tm_isdst = -1;
+}
 
 Ref<Date> Date::copy() const
 {
@@ -63,61 +178,9 @@ Ref<Date> Date::copy() const
 	return newDate;
 }
 
-void Date::clear()
+Ref<Date> Date::now()
 {
-	struct tm *tm = this;
-	memclr(tm, sizeof(struct tm)); // for paranoid reason
-}
-
-void Date::init(double time)
-{
-	uint64_t seconds = time;
-
-	const int t0 = 719162;         // linear day number (starting from 0) of 1st Jan 1970
-	int tx = t0 + seconds / 86400; // linear day number of this date
-	int d400 = 146097;             // length of the 400 year cycle
-	int d100 = 36524;              // length of the 100 year cycle
-	int d4 = 1461;                 // length of the 4 year cycle
-
-	int tx400 = tx    % d400; // day within the 400 year cycle (starting from 0)
-	int tx100 = tx400 % d100; // day within the 100 year cycle (starting from 0)
-	int tx4   = tx100 % d4;   // day within the   4 year cycle (starting from 0)
-
-	int c400 = tx    / d400; // 400 year cycle number within continuum (starting from 0)
-	int c100 = tx400 / d100; // 100 year cycle number within 400 year cycle (starting from 0)
-	int c4   = tx100 / d4;   //   4 year cycle number within 100 year cycle (starting from 0)
-
-	int century = (c400 * 400 + c100 * 100) / 100;
-	int year = (century * 100) + c4 * 4 + 1;
-	int ty0 = 0; // first day in year within the 4 year cycle (starting from 0)
-	{
-		while (true) {
-			int ty1 = ty0 + daysInYear(year); // first day of next year (starting from 0)
-			if (tx4 < ty1) break;
-			ty0 = ty1;
-			++year;
-		}
-	}
-	int tyx = tx4 - ty0; // day witin this year (starting from 0)
-	tm_mon = 0;
-	int tm0 = 0; // first day of month (within this year)
-	{
-		for (int i = 0; i < 12; ++i) {
-			int tm1 = tm0 + daysInMonth(i, year); // first day of next month
-			if (tyx < tm1) break;
-			++tm_mon;
-			tm0 = tm1;
-		}
-	}
-	tm_mday = tyx - tm0 + 1/*adjust to start with day 1*/;
-	tm_wday = (tx + 1/*adjust to start with Sunday*/) % 7;
-	tm_yday = tyx;
-
-	tm_hour = (seconds / 3600) % 24;
-	tm_min = (seconds / 60) % 60;
-	tm_sec = seconds % 60;
-
-	tm_year = year - 1900;
+	return Date::create(System::now());
 }
 
 Ref<Date> Date::localTime()
@@ -156,6 +219,24 @@ String Date::toString() const
 	return Format()
 		<< dec(tm_year + 1900, 4) << "-" << dec(tm_mon + 1, 2) << "-" << dec(tm_mday, 2)
 		<< "T" << dec(tm_hour, 2) << dec(tm_min, 2) << dec(tm_sec, 2) << tz;
+}
+
+String Date::monthName() const
+{
+	const char *names[] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+	int i = tm_mon;
+	if (i < 0) i = 0;
+	else if (i > 11) i = 11;
+	return names[i];
+}
+
+String Date::dayName() const
+{
+	const char *names[] = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+	int i = tm_wday;
+	if (i < 0) i = 0;
+	else if (i > 6) i = 6;
+	return names[i];
 }
 
 } // namespace fkit
--- core/tests/testDateTime.cpp
+++ core/tests/testDateTime.cpp
@@ -5,20 +5,30 @@
 
 using namespace fkit;
 
+namespace fkit {
+	bool leapYear(int y);
+}
+
 int main()
 {
+	if (false) {
+		fout() << "Compute linear day number of 1st Jan 1970..." << nl;
+		int n = 0;
+		for (int y = 1969; y >= 0; --y) n += 365 + leapYear(y);
+		fout() << n << nl;
+	}
 	{
 		fout() << "Basic date formatting..." << nl;
 
 		double t = System::now();
-		String s1 = str(Date::create(Date::create(t)->time()));
-		String s2 = str(Date::create(t));
+		Ref<Date> d1 = Date::create(Date::create(t)->time());
+		Ref<Date> d2 = Date::create(t);
 
-		fout("%%\n") << s1;
-		fout("%%\n") << s2;
-		fout("%%\n") << Date::create(t)->localTime();
+		fout() << d1->dayName() << ", " << d1 << nl;
+		fout() << d2->dayName() << ", " << d2 << nl;
+		fout() << Date::create(t)->localTime() << nl;
 
-		check(s1 == s2);
+		check(str(d1) == str(d2));
 	}
 
 	{
@@ -30,7 +40,56 @@ int main()
 		fout() << "month: " << date->month() << nl;
 		fout() << "day: " << date->day() << nl;
 
-		check(date->year() == 1970 && date->month() == 0 && date->day() == 1 && date->valid());
+		check(date->year() == 1970 && date->month() == 1 && date->day() == 1 && date->valid());
+	}
+
+	{
+		fout() << "Handling broken down times..." << nl;
+
+		struct TestDate { int day; int month; int year; int hour; int minutes; int seconds; };
+		TestDate tests[] = {
+			{  1,  1, 2250,  0,  0,  0 },
+		    { 12,  9, 2013,  0,  0,  0 },
+			{  3,  9, 2012, 11, 22, 33 },
+			{  1,  7, 1981,  0,  0,  0 },
+			{  1,  1, 1970,  0,  0,  0 },
+			{ 24,  7, 1969, 16, 50, 35 },
+			{  1,  1, 1968,  0,  0,  0 },
+			{  1,  1, 1967,  0,  0,  0 },
+			{  1,  7, 1967,  0,  0,  0 },
+			{  1,  1, 1900,  0,  0,  0 },
+			{  1,  1,    1,  0,  0,  0 }
+		};
+		for (int i = 0; i < sizeof(tests)/sizeof(tests[0]); ++i) {
+			TestDate *d = &tests[i];
+			Ref<Date> date0 = Date::create(d->year, d->month, d->day, d->hour, d->minutes, d->seconds);
+			Ref<Date> date1 = Date::create(date0->time());
+			fout() << int64_t(date0->time()) << nl;
+			fout() << "  " << date0->dayName() << ", " << date0 << " == " << date1->dayName() << ", " << date1 << nl;
+			check(date0->time() == date1->time());
+			check(date0->toString() == date1->toString());
+		}
+
+		if (false) {
+			double t = Date::create(9999, 12, 31)->time();
+			while (true) {
+				Ref<Date> d1 = Date::create(t);
+				Ref<Date> d2 = Date::create(d1->year(), d1->month() + 1, d1->day());
+				fout() << "t = " << int64_t(t) << "  " << d1 << nl;
+				if (d1->year() != d2->year() || d1->month() != d2->month() || d1->day() != d2->day()) {
+					fout() << d1 << " != " << d2 << nl;
+					check(false);
+				}
+				if (d1->time() != d2->time()) {
+					fout() << d1 << " != " << d2 << nl;
+					fout() << "  " << int64_t(d1->time()) << " != " << int64_t(d2->time()) << nl;
+					fout() << "  " << int64_t(d1->time()) - int64_t(d2->time()) << nl;
+					check(false);
+				}
+				if (d1->year() == 1 && d1->month() == 1 && d1->day() == 1) break;
+				t -= 86400;
+			}
+		}
 	}
 
 	return 0;
