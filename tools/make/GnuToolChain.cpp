ec1564f71fa8b44d06af6271420a40c1bb8e5315
Frank Mertens <frank@cyblogic.de>
2013-11-02 18:00:02 +0100

    Improved speed of tool chain instantiation by using env MACHTYPE.
    Signed-off-by: Frank Mertens <frank@cyblogic.de>

--- tools/make/GnuToolChain.cpp
+++ tools/make/GnuToolChain.cpp
@@ -24,6 +24,8 @@ GnuToolChain::GnuToolChain(String execPath)
 
 String GnuToolChain::queryMachine(String execPath)
 {
+	String machType = Process::env("MACHTYPE");
+	if (machType != "") return machType;
 	return Process::start(machineCommand(execPath), Process::ForwardOutput)->lineOut()->readLine();
 }
 
