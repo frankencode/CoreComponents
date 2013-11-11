2993bdf02c6cf99839f74ecfe98017bd59a01245
Frank Mertens <frank@cyblogic.de>
2013-11-11 20:31:50 +0100

    fluxpack: Added "-explode" option
    Signed-off-by: Frank Mertens <frank@cyblogic.de>

--- tools/pack/main.cpp
+++ tools/pack/main.cpp
@@ -9,6 +9,9 @@
 
 #include <flux/stdio.h>
 #include <flux/Config.h>
+#include <flux/Date.h>
+#include <flux/File.h>
+#include <flux/Dir.h>
 #include <flux/Archive.h>
 
 using namespace flux;
@@ -19,8 +22,8 @@ int main(int argc, char **argv)
 		Ref<YasonObject> prototype = YasonObject::create();
 		prototype->insert("list", false);
 		prototype->insert("status", false);
-		prototype->insert("bundle", false);
 		prototype->insert("explode", false);
+		prototype->insert("bundle", false);
 		Ref<Config> options = Config::read(argc, argv, prototype);
 		if (options->value("list")) {
 			for (int i = 0; i < options->arguments()->size(); ++i) {
@@ -31,17 +34,67 @@ int main(int argc, char **argv)
 				}
 			}
 		}
+		else if (options->value("status")) {
+			for (int i = 0; i < options->arguments()->size(); ++i) {
+				String path = options->arguments()->at(i);
+				Ref<Archive> archive = Archive::open(path);
+				for (Ref<ArchiveEntry> entry; archive->read(&entry);) {
+					Format status = fout();
+					status << oct(entry->mode()) << " ";
+					if (entry->userName() != "") status << entry->userName() << " ";
+					else status << entry->userId() << " ";
+					if (entry->groupName() != "") status << entry->groupName() << " ";
+					else status << entry->groupId() << " ";
+					status << entry->size() << " ";
+					status << Date::create(entry->lastModified()) << " ";
+					status << entry->path() << nl;
+				}
+			}
+		}
+		else if (options->value("explode")) {
+			for (int i = 0; i < options->arguments()->size(); ++i) {
+				String path = options->arguments()->at(i);
+				Ref<Archive> archive = Archive::open(path);
+				for (Ref<ArchiveEntry> entry; archive->read(&entry);) {
+					fout() << entry->path() << nl;
+					if (entry->type() == ArchiveEntry::Directory) {
+						if (!Dir::create(entry->path(), entry->mode()))
+							FLUX_SYSTEM_EXCEPTION;
+					}
+					else if (entry->type() == ArchiveEntry::Link) {
+						if (!File::link(entry->linkPath(), entry->path()))
+							FLUX_SYSTEM_EXCEPTION;
+					}
+					else if (entry->type() == ArchiveEntry::Symlink) {
+						if (!File::symlink(entry->linkPath(), entry->path()))
+							FLUX_SYSTEM_EXCEPTION;
+					}
+					else if (entry->type() == ArchiveEntry::Regular) {
+						if (!File::create(entry->path(), entry->mode()))
+							FLUX_SYSTEM_EXCEPTION;
+						Ref<File> file = File::open(entry->path(), File::WriteOnly);
+						file->write(ByteArray::wrap(archive->data()->data() + entry->offset(), entry->size()));
+					}
+				}
+			}
+		}
+		else if (options->value("bundle")) {
+			/*for (int i = 0; i < options->arguments()->size(); ++i) {
+				String path = options->arguments()->at(i);
+
+			}*/
+		}
 	}
 	catch (HelpException &) {
 		fout(
-			"Usage: %% [OPTION]... [FILE]...\n"
-			"Pack, unpack or list the contents of an archive file.\n"
+			"Usage: %% [OPTION]... [FILE|DIR]...\n"
+			"Pack, unpack or list the contents of archive files.\n"
 			"\n"
 			"Options:\n"
-			"  -list     list contents of an archive file\n"
-			"  -status   show archived file status of each archive member\n"
-			"  -bundle   pack files together to an archive file\n"
-			"  -explode  unpack an archive file\n"
+			"  -list     list contents of given archive files\n"
+			"  -status   show archived file status for each archive member\n"
+			"  -explode  unpack archive files\n"
+			"  -bundle   pack each directory into an archive file\n"
 		) << String(argv[0])->fileName();
 		return 1;
 	}
