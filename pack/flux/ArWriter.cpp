bb2d8a7e81b7f3df1dad13bcc2776cd4b1c9171e
Frank Mertens <frank@cyblogic.de>
2013-12-03 21:09:09 +0100

    libfluxpack: Added ArWriter.
    Signed-off-by: Frank Mertens <frank@cyblogic.de>

---
+++ pack/flux/ArWriter.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2013 Frank Mertens.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or (at your option) any later version.
+ */
+
+#include <flux/File.h>
+#include <flux/str.h>
+#include "ArWriter.h"
+
+namespace flux
+{
+
+ArWriter::ArWriter(Stream *sink)
+	: sink_(sink)
+{}
+
+Ref<ArWriter> ArWriter::open(Stream *sink)
+{
+	return new ArWriter(sink);
+}
+
+void ArWriter::appendFile(String path)
+{
+	Ref<File> file = File::open(path);
+	Ref<FileStatus> status = file->status();
+
+	Ref<StringList> headerFields = StringList::create();
+
+	String pathField(16, ' ');
+	*pathField = *path;
+	headerFields->append(pathField);
+
+	String lastModifiedField(12, ' ');
+	*lastModifiedField = *dec(status->st_mtime);
+	headerFields->append(lastModifiedField);
+
+	String userIdField(6, ' ');
+	*userIdField = *dec(status->ownerId());
+	headerFields->append(userIdField);
+
+	String groupIdField(6, ' ');
+	*groupIdField = *dec(status->groupId());
+	headerFields->append(groupIdField);
+
+	String modeField(8, ' ');
+	*modeField = *oct(status->mode());
+	headerFields->append(modeField);
+
+	String sizeField(10, ' ');
+	*sizeField = *dec(status->size());
+	headerFields->append(sizeField);
+
+	String magic(2);
+	magic->byteAt(0) = 0x60;
+	magic->byteAt(1) = 0x0a;
+	headerFields->append(magic);
+
+	sink_->write(headerFields);
+	file->transfer(status->size(), sink_);
+}
+
+
+} // namespace flux
