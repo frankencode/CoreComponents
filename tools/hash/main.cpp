dfa1822c6aff35e01d14f138734d9c46cba2d05b
Frank Mertens <frank@cyblogic.de>
2014-05-31 00:25:45 +0200

    fluxmd5/sha1: accept input from stdin
    Signed-off-by: Frank Mertens <frank@cyblogic.de>

--- tools/hash/main.cpp
+++ tools/hash/main.cpp
@@ -25,21 +25,26 @@ int main(int argc, char **argv)
 		arguments->validate(VariantMap::create());
 
 		StringList *items = arguments->items();
+		if (items->size() == 0) items->append("");
+
 		for (int i = 0; i < items->size(); ++i) {
 			String path = items->at(i);
 			Ref<HashSum> hashSum;
 			if (toolName->contains("sha1")) hashSum = Sha1::create();
 			else hashSum = Md5::create();
 			Ref<HashMeter> hashMeter = HashMeter::open(hashSum);
-			File::open(path)->transferAll(hashMeter);
-			fout() << hashMeter->finish()->hex() << "  " << path << nl;
+			Ref<File> source;
+			if (path != "") source = File::open(path);
+			else { source = flux::in(); path = "-"; }
+			source->transferAll(hashMeter);
+			fout() << hashMeter->finish()->hex() << "\t" << path << nl;
 		}
 	}
 	catch (HelpError &) {
 		fout(
 			"Usage: %% [FILE]...\n"
-			"Computes SHA1 sums of files.\n"
-		) << toolName;
+			"Computes %% sums of files.\n"
+		) << toolName << (toolName->contains("sha1") ? "SHA1" : "MD5");
 	}
 	catch (Exception &ex) {
 		ferr() << toolName << ": " << ex.message() << nl;
