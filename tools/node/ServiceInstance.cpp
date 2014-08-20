a9273ab8d68b2fa4a18e7a5385c0b3ba20214bd0
Frank Mertens <frank@cyblogic.de>
2014-08-20 09:56:19 +0200

    Indentation of license headers
    Signed-off-by: Frank Mertens <frank@cyblogic.de>

--- tools/node/ServiceInstance.cpp
+++ tools/node/ServiceInstance.cpp
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2013 Frank Mertens.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or (at your option) any later version.
+ */
+
 #include <flux/MetaObject.h>
 #include "LogConfig.h"
 #include "ServiceInstance.h"
--- util/abnf/flux/abnf/AbnfCompiler.h
+++ util/abnf/flux/abnf/AbnfCompiler.h
@@ -1,11 +1,12 @@
- /*
-  * Copyright (C) 2007-2014 Frank Mertens.
-  *
-  * This program is free software; you can redistribute it and/or
-  * modify it under the terms of the GNU General Public License
-  * as published by the Free Software Foundation; either version
-  * 2 of the License, or (at your option) any later version.
-  */
+/*
+ * Copyright (C) 2007-2014 Frank Mertens.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or (at your option) any later version.
+ */
+
 #ifndef FLUXABNF_ABNFCOMPILER_H
 #define FLUXABNF_ABNFCOMPILER_H
 
