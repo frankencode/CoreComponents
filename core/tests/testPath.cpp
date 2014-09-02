c2da44462824e2a0a97af09e57faaee412c4dc0b
Frank Mertens <frank@cyblogic.de>
2014-09-02 23:27:47 +0200

    Moving core library to BSD license.
    Signed-off-by: Frank Mertens <frank@cyblogic.de>

--- core/tests/testPath.cpp
+++ core/tests/testPath.cpp
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2007-2014 Frank Mertens.
+ *
+ * Use of this source is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ */
+
 #include <flux/stdio.h>
 
 using namespace flux;
