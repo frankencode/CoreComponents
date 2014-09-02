4c2972c10cc24e5048a418a467f5f17d6ee206a9
Frank Mertens <frank@cyblogic.de>
2014-09-02 23:52:18 +0200

    Moving to BSD license.
    Signed-off-by: Frank Mertens <frank@cyblogic.de>

--- util/tools/doc/Registry.h
+++ util/tools/doc/Registry.h
@@ -1,10 +1,9 @@
 /*
- * Copyright (C) 2014 Frank Mertens.
+ * Copyright (C) 2007-2014 Frank Mertens.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version
- * 2 of the License, or (at your option) any later version.
+ * Use of this source is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
  */
 
 #ifndef FLUXDOC_REGISTRY_H
