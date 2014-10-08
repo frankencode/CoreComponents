1a40a0cff8de00c6894c193726e9f6a4d187c6fd
Frank Mertens <frank@cyblogic.de>
2014-10-09 01:38:02 +0200

    New class SignalMaster: thread for waiting for signals.
    Signed-off-by: Frank Mertens <frank@cyblogic.de>

---
+++ core/src/SignalMaster.cpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2007-2014 Frank Mertens.
+ *
+ * Use of this source is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ */
+
+#include <signal.h>
+#include "SignalMaster.h"
+
+namespace flux {
+
+Ref<SignalMaster> SignalMaster::create(SignalSet *listenSet, SignalSet *terminationSet)
+{
+	return new SignalMaster(listenSet, terminationSet);
+}
+
+SignalMaster::SignalMaster(SignalSet *listenSet, SignalSet *terminationSet):
+	listenSet_(listenSet),
+	terminationSet_(terminationSet),
+	receivedSignals_(SignalChannel::create())
+{
+	if (!listenSet_) listenSet_ = SignalSet::createFull();
+	if (!terminationSet_) {
+		terminationSet_ = SignalSet::createEmpty();
+		terminationSet_->insert(SIGINT);
+		terminationSet_->insert(SIGTERM);
+	}
+}
+
+void SignalMaster::run()
+{
+	int signal = 0;
+	while (true) {
+		int error = ::sigwait(listenSet_->rawSet(), &signal);
+		if (error != 0) FLUX_SYSTEM_DEBUG_ERROR(error);
+		receivedSignals_->pushBack(signal);
+		if (terminationSet_->contains(signal)) break;
+	}
+}
+
+} // namespace flux
