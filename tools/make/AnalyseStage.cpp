6218358b2e5708e5277477104d0d41d7a3c85dad
Frank Mertens <frank@cyblogic.de>
2014-11-10 10:34:03 +0100

    Fix for prequisite generation (moved to before source globbing)
    Signed-off-by: Frank Mertens <frank@cyblogic.de>

--- tools/make/AnalyseStage.cpp
+++ tools/make/AnalyseStage.cpp
@@ -54,7 +54,7 @@ bool AnalyseStage::run()
         if (plan()->options() & BuildPlan::Verbose || job->status() != 0)
             fout() << "# " << shell()->beautify(job->command()) << nl;
         if (job->status() != 0) {
-            ferr() << "# " << job->outputText();
+            ferr() << job->outputText();
             status_ = job->status();
             return success_ = false;
         }
