0420c62e3162b2b8e202a7f39e0221e4f829ddb0
Frank Mertens <frank@cyblogic.de>
2015-01-11 22:57:27 +0100

    Added support for reading the routing table
    Signed-off-by: Frank Mertens <frank@cyblogic.de>

---
+++ net/src/RouteInfo.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2007-2014 Frank Mertens.
+ *
+ * Use of this source is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ */
+
+#ifndef FLUX_ROUTEINFO_H
+#define FLUX_ROUTEINFO_H
+
+#include <flux/net/SocketAddress>
+
+namespace flux {
+namespace net {
+
+class RouteInfo;
+
+typedef List< Ref<RouteInfo> > RouteInfoList;
+
+class RouteInfo: public Object
+{
+public:
+    static Ref<RouteInfoList> queryTable();
+
+    inline SocketAddress *source() const { return source_; }
+    inline int sourceLength() const { return sourceLength_; }
+    inline SocketAddress *destination() const { return destination_; }
+    inline int destinationLength() const { return destinationLength_; }
+    inline SocketAddress *gateway() const { return gateway_; }
+    inline String inputInterface() const { return inputInterface_; }
+    inline String outputInterface() const { return outputInterface_; }
+    inline int metric() const { return metric_; }
+    inline int priority() const { return priority_; }
+
+private:
+    inline static Ref<RouteInfo> create() { return new RouteInfo; }
+    RouteInfo() {}
+
+    Ref<SocketAddress> source_;
+    int sourceLength_;
+    Ref<SocketAddress> destination_;
+    int destinationLength_;
+    Ref<SocketAddress> gateway_;
+    String inputInterface_;
+    String outputInterface_;
+    int metric_;
+    int priority_;
+};
+
+}} // namespace flux::net
+
+#endif // FLUX_ROUTEINFO_H
---
+++ net/tests/testRouteInfo.cpp
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2007-2014 Frank Mertens.
+ *
+ * Use of this source is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ */
+
+#include <flux/Format>
+#include <flux/stdio>
+#include <flux/net/RouteInfo>
+
+using namespace flux;
+using namespace flux::net;
+
+int main()
+{
+    Ref<RouteInfoList> list = RouteInfo::queryTable();
+
+    for (int i = 0; i < list->count(); ++i) {
+        RouteInfo *info = list->at(i);
+        Format line;
+        if (info->destinationLength() == 0 && info->sourceLength() == 0) line << "default";
+        if (info->gateway()) line << "via" << info->gateway();
+        if (info->destination())
+            line << String(Format("%%/%%") << info->destination() << info->destinationLength());
+        if (info->outputInterface() != "")
+            line << "out" << info->outputInterface();
+        if (info->inputInterface() != "")
+            line << "in" << info->inputInterface();
+        if (info->source())
+            line << "src" << String(Format("%%/%%") << info->source() << info->sourceLength());
+        line << info->priority();
+        fout() << line->join(" ") << nl;
+    }
+
+    return 0;
+}
