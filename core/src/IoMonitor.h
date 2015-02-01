b8ab35075568d1f6d2d0bcb6cd00081374abd31a
Frank Mertens <frank@cyblogic.de>
2015-02-01 22:39:49 +0100

    Updated copyright statement
    Signed-off-by: Frank Mertens <frank@cyblogic.de>

--- core/src/IoMonitor.h
+++ core/src/IoMonitor.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2014 Frank Mertens.
+ * Copyright (C) 2007-2015 Frank Mertens.
  *
  * Use of this source is governed by a BSD-style license that can be
  * found in the LICENSE file.
--- core/src/SyntaxSugar.sh
+++ core/src/SyntaxSugar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ##
- # Copyright (C) 2007-2014 Frank Mertens.
+ # Copyright (C) 2007-2015 Frank Mertens.
  #
  # Use of this source is governed by a BSD-style license that can be
  # found in the LICENSE file.
