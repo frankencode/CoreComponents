364081db10bebabec44287650fa4c0a0818f5056
Frank Mertens <frank@cyblogic.de>
2015-08-25 22:48:10 +0200

    A few fixes to IoMonitor: allow to wait for-ever

--- core/src/IoMonitor.h
+++ core/src/IoMonitor.h
@@ -21,31 +21,31 @@ namespace flux {
 class IoEvent: public Object
 {
 public:
-    enum Activity {
+    enum Type {
         ReadyRead = POLLIN,
         ReadyWrite = POLLOUT,
         ReadyAccept = ReadyRead
     };
 
     inline SystemStream *stream() const { return stream_; }
-    inline int activity() const { return activity_; }
+    inline int type() const { return type_; }
 
 private:
     friend class IoMonitor;
 
-    inline static Ref<IoEvent> create(int index, SystemStream *stream, int activity) {
-        return new IoEvent(index, stream, activity);
+    inline static Ref<IoEvent> create(int index, SystemStream *stream, int type) {
+        return new IoEvent(index, stream, type);
     }
 
-    IoEvent(int index, SystemStream *stream, int activity):
+    IoEvent(int index, SystemStream *stream, int type):
         index_(index),
         stream_(stream),
-        activity_(activity)
+        type_(type)
     {}
 
     int index_;
     Ref<SystemStream> stream_;
-    int activity_;
+    int type_;
 };
 
 typedef Array<IoEvent *> IoActivity;
@@ -57,7 +57,7 @@ class IoMonitor: public Object
 public:
     static Ref<IoMonitor> create(int maxCount = 0);
 
-    IoEvent *addEvent(SystemStream *stream, int activity);
+    IoEvent *addEvent(SystemStream *stream, int type);
     void removeEvent(IoEvent *event);
 
     Ref<IoActivity> wait(double timeout);
