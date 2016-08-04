77bd4d9f13e0052f8204b223afb86d3177337bf9
Frank Mertens <frank@cyblogic.de>
2016-08-04 12:53:35 +0200

    Hot fix

--- core/src/Bundle.h
+++ core/src/Bundle.h
@@ -14,13 +14,16 @@
 
 #include <cc/String>
 
-namespace cc {
-
 #define CC_XSTR(s) CC_STR(s)
 #define CC_STR(s) #s
 
-#define CC_BUNDLE_LOOKUP(relPath) Bundle::lookup(relPath, cc::StringList::create() << cc::Bundle::prefix(CC_XSTR(CCBUILD_BUNDLE_PREFIX)) << cc::Bundle::exePrefix() << String(__FILE__)->reducePath())
+/// Lookup a path of a bundled ressource
+#define CC_BUNDLE_LOOKUP(relPath) cc::Bundle::lookup(relPath, cc::StringList::create() << cc::Bundle::prefix(CC_XSTR(CCBUILD_BUNDLE_PREFIX)) << cc::Bundle::exePrefix() << cc::String(__FILE__)->reducePath())
+
+/// Version of this application or library bundle (low-level C string)
 #define CC_BUNDLE_VERSION CC_XSTR(CCBUILD_BUNDLE_VERSION)
+
+namespace cc {
 
 template<class>
 class Singleton;
