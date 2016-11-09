7125dd6ef8d8906f5ecfe004966ed97aa6ad95f9
Frank Mertens <frank@cyblogic.de>
2016-11-09 23:02:01 +0100

    Added documentation for LineSource

--- core/src/LineSource.cc
+++ core/src/LineSource.cc
@@ -10,21 +10,21 @@
 
 namespace cc {
 
-Ref<LineSource> LineSource::open(ByteArray *buf) {
-    return new LineSource(0, buf);
+Ref<LineSource> LineSource::open(ByteArray *buffer) {
+    return new LineSource(0, buffer);
 }
 
-Ref<LineSource> LineSource::open(Stream *stream, ByteArray *buf) {
-    return new LineSource(stream, buf);
+Ref<LineSource> LineSource::open(Stream *stream, ByteArray *buffer) {
+    return new LineSource(stream, buffer);
 }
 
-LineSource::LineSource(Stream *stream, ByteArray *buf):
+LineSource::LineSource(Stream *stream, ByteArray *buffer):
     stream_(stream),
-    buf_(buf),
+    buffer_(buffer),
     eoi_(false),
     i_(0), n_(0)
 {
-    if (!buf) buf_ = ByteArray::allocate(0x1000);
+    if (!buffer) buffer_ = ByteArray::allocate(0x1000);
 }
 
 bool LineSource::read(String *line)
@@ -39,26 +39,26 @@ bool LineSource::read(String *line)
     while (true) {
         if (i_ < n_) {
             int i0 = i_;
-            i_ = findEol(buf_, n_, i_);
+            i_ = findEol(buffer_, n_, i_);
             if (i_ < n_) {
                 if (backlog) {
-                    backlog->append(buf_->copy(i0, i_));
+                    backlog->append(buffer_->copy(i0, i_));
                     *line = backlog->join();
                 }
                 else {
-                    *line = buf_->copy(i0, i_);
+                    *line = buffer_->copy(i0, i_);
                 }
-                i_ = skipEol(buf_, n_, i_);
+                i_ = skipEol(buffer_, n_, i_);
                 return true;
             }
             if (!backlog)
                 backlog = StringList::create();
-            backlog->append(buf_->copy(i0, i_));
+            backlog->append(buffer_->copy(i0, i_));
         }
 
         if (!stream_) break;
 
-        n_ = stream_->read(buf_);
+        n_ = stream_->read(buffer_);
         if (n_ == 0) break;
         i_ = 0;
     }
@@ -82,24 +82,24 @@ String LineSource::readLine()
 String LineSource::pendingData() const
 {
     if (eoi_) return String();
-    return buf_->copy(i_, n_);
+    return buffer_->copy(i_, n_);
 }
 
-int LineSource::findEol(ByteArray *buf, int n, int i) const
+int LineSource::findEol(ByteArray *buffer, int n, int i) const
 {
     for (; i < n; ++i) {
-        char ch = buf->at(i);
+        char ch = buffer->at(i);
         if (ch == '\n' || ch == '\r' || ch == '\0')
             break;
     }
     return i;
 }
 
-int LineSource::skipEol(ByteArray *buf, int n, int i) const
+int LineSource::skipEol(ByteArray *buffer, int n, int i) const
 {
-    if (i < n) if (buf->at(i) == '\r') ++i;
-    if (i < n) if (buf->at(i) == '\n') ++i;
-    if (i < n) if (buf->at(i) == '\0') ++i;
+    if (i < n) if (buffer->at(i) == '\r') ++i;
+    if (i < n) if (buffer->at(i) == '\n') ++i;
+    if (i < n) if (buffer->at(i) == '\0') ++i;
     return i;
 }
 
--- core/src/LineSource.h
+++ core/src/LineSource.h
@@ -19,25 +19,47 @@ namespace cc {
 class LineSource: public Source<String>
 {
 public:
-    static Ref<LineSource> open(ByteArray *buf);
-    static Ref<LineSource> open(Stream *stream, ByteArray *buf = 0);
+    /** Open a new line source feed from a memory buffer
+      * \param buffer input buffer
+      * \return new object instance
+      */
+    static Ref<LineSource> open(ByteArray *buffer);
 
+    /** Open a new line source feed from an input stream
+      * \param stream input stream
+      * \param buffer input buffer (optional)
+      * \return new object instance
+      */
+    static Ref<LineSource> open(Stream *stream, ByteArray *buffer = 0);
+
+    /// Input stream used to read lines from
     inline Stream *stream() const { return stream_; }
-    inline ByteArray *buf() const { return buf_; }
 
+    /// Input buffer used to cache input data
+    inline ByteArray *buffer() const { return buffer_; }
+
+    /** Read next line
+      * \param line returns the next line (if not end of input)
+      * \return true if not end of input, false otherwise
+      */
     bool read(String *line);
+
+    /** Read next line
+      * \return next line (equals "" on end of input)
+      */
     String readLine();
 
+    /// Return the remaining data from the input buffer
     String pendingData() const;
 
 private:
-    LineSource(Stream *stream, ByteArray *buf);
+    LineSource(Stream *stream, ByteArray *buffer);
 
-    int findEol(ByteArray *buf, int n, int i) const;
-    int skipEol(ByteArray *buf, int n, int i) const;
+    int findEol(ByteArray *buffer, int n, int i) const;
+    int skipEol(ByteArray *buffer, int n, int i) const;
 
     Ref<Stream> stream_;
-    String buf_;
+    Ref<ByteArray> buffer_;
     bool eoi_;
     int i_, n_;
 };
