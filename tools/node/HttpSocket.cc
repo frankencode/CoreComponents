476efe7c8d983ff351cbd8e1d65ed7131137a72f
Frank Mertens <frank@cyblogic.de>
2016-11-10 13:31:20 +0100

    Fix: typo

--- tools/node/HttpSocket.cc
+++ tools/node/HttpSocket.cc
@@ -58,7 +58,7 @@ HttpSocket::~HttpSocket()
     if (mode_ & Secure)
     {
         if (mode_ & Open) {
-            int ret = gnutls_bye(session_, GNUTLS_SHUT_WR));
+            int ret = gnutls_bye(session_, GNUTLS_SHUT_WR);
             if (ret != GNUTLS_E_SUCCESS)
                 CCNODE_ERROR() << peerAddress << ": " << gnutls_strerror(ret) << nl;
         }
