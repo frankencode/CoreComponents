b6814ed5bee41cdcd2fa645b0125d5f31e3503e8
Frank Mertens <frank@cyblogic.de>
2016-12-21 00:38:05 +0100

    Minor API cleanup and more doc comments

--- core/src/Version.h
+++ core/src/Version.h
@@ -22,34 +22,56 @@ class Variant;
 class Version
 {
 public:
+    /// Construct an invalid version version tuple (0, 0, 0)
     Version():
         major_(0), minor_(0), patch_(0)
     {}
 
+    /// Construct a new version software version tuple (\a major, \a minor, \a patch)
     Version(int major, int minor, int patch = 0):
         major_(major), minor_(minor), patch_(patch)
     {}
 
+    /// Construct a version from a C-string \a s
     Version(const char* s);
+
+    /// Construct a version by parsing a string \a s
     Version(const String &s);
+
+    /// Construct a version by parsing a variant \a v
     Version(const Variant &v);
 
+    /// Check if software version \a v is valid
     inline static bool isValid(Version v) { return major(v) > 0 || minor(v) > 0; }
 
+    /// Return the major version of the software version tuple \a v
     inline static int major(Version v) { return v.major_; }
+
+    /// Return the minor version of the software version tuple \a v
     inline static int minor(Version v) { return v.minor_; }
+
+    /// Return the patch version of the software version tuple \a v
     inline static int patch(Version v) { return v.patch_; }
 
+    /// Compare if this object is lower than \a b
     inline bool operator< (const Version &b) { return n() <  b.n(); }
+
+    /// Compare if this object is lower or equal than \a b
     inline bool operator<=(const Version &b) { return n() <= b.n(); }
+
+    /// Compare if this object is greater than \a b
     inline bool operator> (const Version &b) { return n() >  b.n(); }
+
+    /// Compare if this object is greater or equal than \a b
     inline bool operator>=(const Version &b) { return n() >= b.n(); }
+
+    /// Compare if this object equals \a b
     inline bool operator==(const Version &b) { return n() == b.n(); }
+
+    /// Compare if this object is unequal \a b
     inline bool operator!=(const Version &b) { return n() != b.n(); }
 
-    inline Version *operator->() { return this; }
-    inline const Version *operator->() const { return this; }
-
+    /// Conersion to string operator
     inline operator String() const;
 
 private:
@@ -66,6 +88,7 @@ private:
     uint16_t patch_;
 };
 
+/// Stringify a software version tuple \a v
 String str(Version v);
 
 inline Version::operator String() const { return str(*this); }
