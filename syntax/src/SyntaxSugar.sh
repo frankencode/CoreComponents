150078972c0b51493f3f9875a2b7b6682246942f
Frank Mertens <frank@cyblogic.de>
2017-07-08 00:05:46 +0200

    Updated license headers

--- syntax/src/SyntaxSugar.sh
+++ syntax/src/SyntaxSugar.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ##
- # Copyright (C) 2007-2015 Frank Mertens.
+ # Copyright (C) 2007-2017 Frank Mertens.
  #
- # Use of this source is governed by a BSD-style license that can be
- # found in the LICENSE file.
+ # Distribution and use is allowed under the terms of the zlib license
+ # (see cc/LICENSE-zlib).
  #
  ##
 
