7084e37afea7a53174a3c3ee81e0564b6a8ecf7c
Frank Mertens <frank@cyblogic.de>
2017-11-06 01:23:13 +0100

    Split off type-unspecific parts of Property<T> class, improved operator support

---
+++ core/src/PropertyBinding.cc
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2007-2017 Frank Mertens.
+ *
+ * Distribution and use is allowed under the terms of the zlib license
+ * (see cc/LICENSE-zlib).
+ *
+ */
+
+#include <cc/Set>
+#include <cc/PropertyBinding>
+
+namespace cc {
+
+thread_local PropertyBinding *PropertyBinding::activeInstance_ = 0;
+
+PropertyBinding::PropertyBinding(bool dirty):
+    dirty_(dirty)
+{}
+
+PropertyBinding::~PropertyBinding()
+{
+    clearDependencies();
+    clearSubscribers();
+}
+
+void PropertyBinding::preAccess() const
+{
+    if (dirty_) {
+        const_cast<PropertyBinding *>(this)->dirty_ = false;
+        const_cast<PropertyBinding *>(this)->cascade();
+    }
+
+    if ((activeInstance_) && (activeInstance_ != this)) {
+        if (activeInstance_->dependencies()->insert(const_cast<PropertyBinding *>(this)))
+            subscribers()->insert(activeInstance_);
+    }
+}
+
+PropertyBinding::Association *PropertyBinding::dependencies() const
+{
+    if (!dependencies_) dependencies_ = Association::create();
+    return dependencies_;
+}
+
+PropertyBinding::Association *PropertyBinding::subscribers() const
+{
+    if (!subscribers_) subscribers_ = Association::create();
+    return subscribers_;
+}
+
+bool PropertyBinding::hasConsumers() const
+{
+    return
+        (subscribers_ && subscribers_->count() > 0) ||
+        valueChanged->hasListeners();
+}
+
+void PropertyBinding::clearDependencies()
+{
+    if (!dependencies_) return;
+
+    for (PropertyBinding *other: dependencies_)
+        other->subscribers_->remove(this);
+    dependencies_ = nullptr;
+}
+
+void PropertyBinding::clearSubscribers()
+{
+    if (!subscribers_) return;
+
+    for (PropertyBinding *other: subscribers_)
+        other->dependencies_->remove(this);
+    subscribers_ = nullptr;
+}
+
+void PropertyBinding::notify()
+{
+    if (subscribers_) {
+        for (PropertyBinding *other: subscribers_) {
+            if (other->hasConsumers()) {
+                other->dirty_ = false;
+                other->evaluate();
+            }
+            else {
+                other->dirty_ = true;
+            }
+        }
+    }
+    valueChanged->notify();
+}
+
+void PropertyBinding::cascade()
+{
+    class Activator {
+    public:
+        Activator(PropertyBinding *instance): savedActiveInstance_(PropertyBinding::activeInstance_) {
+            PropertyBinding::activeInstance_ = instance;
+        }
+        ~Activator() {
+            PropertyBinding::activeInstance_ = savedActiveInstance_;
+        }
+    private:
+        PropertyBinding *savedActiveInstance_;
+    };
+
+    Activator activator(this);
+    evaluate();
+}
+
+} // namespace cc
