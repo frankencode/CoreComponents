1fae7894cd134ebeeb2b3a398f3da39deb947340
Frank Mertens <frank@cyblogic.de>
2017-12-18 02:41:18 +0100

    Container classes: semantic refactoring of the binary tree hierarchy

--- core/src/BinaryTree.cc
+++ core/src/BinaryTree.cc
@@ -24,7 +24,7 @@ void BinaryTree::attach(BinaryNode *kp, BinaryNode *k, bool left)
         k->right_ = 0;
     }
     else {
-        setRoot(k);
+        root_ = k;
         k->parent_ = 0;
         k->left_ = 0;
         k->right_ = 0;
@@ -48,7 +48,7 @@ BinaryNode *BinaryTree::detach(BinaryNode *k)
         if (kc != 0) kc->parent_ = kp;
     }
     else {
-        setRoot(0);
+        root_ = 0;
     }
     changed(kp, k, left, false);
     return k;
@@ -69,7 +69,7 @@ void BinaryTree::replace(BinaryNode *ki, BinaryNode *kl)
             kp->right_ = kl;
     }
     else {
-        setRoot(kl);
+        root_ = kl;
     }
     if (ki->left_)
         ki->left_->parent_ = kl;
@@ -155,5 +155,131 @@ BinaryNode *BinaryTree::max(BinaryNode *k)
     }
     return k2;
 }
+
+/** Perform an equivalent tree transformation.
+  *
+  * Rotate left:
+  *
+  *    k1      =>      k2
+  *   /  \            /  \
+  *  a    k2         k1   c
+  *      /  \       /  \
+  *     b    c     a    b
+  *
+  * Rotate right:
+  *
+  *      k1    =>    k2
+  *     /  \        /  \
+  *    k2   c      a    k1
+  *   /  \             /  \
+  *  a    b           b    c
+  */
+void BinaryTree::rotate(BinaryNode *k1, bool left)
+{
+    BinaryNode *k2 = left ? k1->right_ : k1->left_;
+    if (k1->parent_) {
+        if (k1->parent_->left_ == k1)
+            k1->parent_->left_ = k2;
+        else
+            k1->parent_->right_ = k2;
+    }
+    else {
+        root_ = k2;
+    }
+    k2->parent_ = k1->parent_;
+    k1->parent_ = k2;
+    if (left) {
+        k1->right_ = k2->left_;
+        if (k2->left_)
+            k2->left_->parent_ = k1;
+        k2->left_ = k1;
+    }
+    else {
+        k1->left_ = k2->right_;
+        if (k2->right_)
+            k2->right_->parent_ = k1;
+        k2->right_ = k1;
+    }
+    rotated(k1, left);
+}
+
+void AvlBalance::restore(BinaryTree *tree, BinaryNode *kp, bool left, bool attached)
+{
+    if (!kp) return;
+
+    int delta = 2 * left - 1;
+    BinaryNode *k = kp;
+    while (true) {
+        k->balance_ += attached ? delta : -delta;
+        if ((k->balance_ == 2) || (k->balance_ == -2))
+            k = rebalance(tree, k);
+        if ( (attached ? (k->balance_ == 0) : (k->balance_ != 0)) ||
+             (!k->parent_) )
+            break;
+        delta = 2 * (k->parent_->left_ == k) - 1;
+        k = k->parent_;
+    }
+}
+
+BinaryNode *AvlBalance::rebalance(BinaryTree *tree, BinaryNode *k1)
+{
+    if (k1->balance_ == 2) {
+        BinaryNode *k2 = k1->left_;
+        if (k2->balance_ == -1) {
+            // -- case Ia->III
+            BinaryNode *k3 = k2->right_;
+            k1->balance_ = -(k3->balance_ == 1);
+            k2->balance_ = (k3->balance_ == -1);
+            k3->balance_ = 0;
+            tree->rotate(k2, true);
+            tree->rotate(k1, false);
+        }
+        else {
+            // -- case Ib, Ic
+            k1->balance_ = (k2->balance_ <= 0);
+            k2->balance_ = k2->balance_ - 1;
+            tree->rotate(k1, false);
+        }
+    }
+    else {
+        BinaryNode *k2 = k1->right_;
+        if (k2->balance_ == 1) {
+            // -- case IIc->IV
+            BinaryNode *k3 = k2->left_;
+            k1->balance_ = (k3->balance_ == -1);
+            k2->balance_ = -(k3->balance_ == 1);
+            k3->balance_ = 0;
+            tree->rotate(k2, false);
+            tree->rotate(k1, true);
+        }
+        else {
+            // -- case IIa, IIb
+            k1->balance_ = -(k2->balance_ >= 0);
+            k2->balance_ = k2->balance_ + 1;
+            tree->rotate(k1, true);
+        }
+    }
+    return k1->parent_;
+}
+
+#ifndef NDEBUG
+
+int AvlBalance::height(BinaryNode *k)
+{
+    if (k == 0) return 0;
+    int h1 = height(k->left_);
+    int h2 = height(k->right_);
+    return (h1 < h2 ? h2 : h1) + 1;
+}
+
+bool AvlBalance::testBalance(BinaryNode *k)
+{
+    if (!k) return true;
+    if (!((k->balance_ == -1) || (k->balance_ == 0) || (k->balance_ == 1))) return false;
+    if ((height(k->left_) - height(k->right_)) != k->balance_) return false;
+    return testBalance(k->left_) && testBalance(k->right_);
+}
+
+#endif // ndef NDEBUG
 
 } // namespace cc
