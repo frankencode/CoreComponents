c0e9c8fc8be47f7087a19e23674d5497a2e2ba5f
Frank Mertens <frank@cyblogic.de>
2017-12-24 23:33:52 +0100

    Correction and fixes for the new Layout container class

--- core/tests/testLayout.cc
+++ core/tests/testLayout.cc
@@ -25,13 +25,36 @@ class SimpleLayout: public TestCase
         layout->append("Item 2", 3);
         layout->append("Item 3", 1);
         layout->append("Item 4", 4);
-        CC_VERIFY(layout->count() == 4);
-        for (int i = 0; i < layout->count(); ++i) {
-            String item;
-            int extent = 0;
-            int pos = layout->getPosAt(i);
-            layout->getItemAt(i, &item, &extent);
-            fout() << item << " (" << extent << "): " << pos << nl;
+        layout->append("Item 5", 1);
+        CC_VERIFY(layout->count() == 5);
+        {
+            for (int i = 0; i < layout->count(); ++i) {
+                String item;
+                int extent = 0;
+                int pos = layout->getPosAt(i);
+                layout->getItemAt(i, &item, &extent);
+                fout() << item << " (" << extent << "): " << pos << ".." << pos + extent - 1  << nl;
+            }
+            fout() << nl;
+        }
+        {
+            int i0 = 0, i1 = 0;
+            int pos = 0;
+            layout->getView(3, 10, &i0, &i1, &pos);
+            CC_ASSERT(i0 == 1 && i1 == 4);
+            for (int i = i0; i < i1; ++i) {
+                String item;
+                int extent = 0;
+                layout->getItemAt(i, &item, &extent);
+                fout() << item << " (" << extent << "): " << pos  << ".." << pos + extent - 1 << nl;
+                pos += extent;
+            }
+            fout() << nl;
+        }
+        {
+            for (auto item: layout)
+                fout() << item << nl;
+            fout() << nl;
         }
     }
 };
