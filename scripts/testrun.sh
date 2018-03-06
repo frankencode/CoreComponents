f4e4584a017cc685abef3eecc2374885b4e818a9
Frank Mertens <frank@cyblogic.de>
2018-03-06 01:11:28 +0100

    Added a test script to test cycle apps on remote targets

---
+++ scripts/testrun.sh
@@ -0,0 +1,50 @@
+#! /bin/bash -x
+
+TEST_APP=exampleWindow
+TEST_ARG=
+TEST_ENV="DISPLAY=:0"
+TEST_STATION=debby-nine
+TEST_OS=${TEST_STATION}
+TEST_USER=${USER}
+BUILD_TYPE=debug
+CC_SOURCE=~/src/cc
+APP_SOURCE=${CC_SOURCE}
+BUILD_DIR=~/build-${TEST_STATION}-${BUILD_TYPE}
+TOOLS_DIR=~/build-${TEST_STATION}-tools
+CCBUILD=${TOOLS_DIR}/ccbuild
+RSYNC=/usr/bin/rsync
+SSH=/usr/bin/ssh
+
+bootstrap_ccbuild() {
+    [ -d $TOOLS_DIR ] || mkdir $TOOLS_DIR
+    pushd $TOOLS_DIR
+    $CC_SOURCE/bootstrap
+    popd
+}
+
+build_app() {
+    [ -d $BUILD_DIR ] || mkdir $BUILD_DIR
+    pushd $BUILD_DIR
+    $CCBUILD -$BUILD_TYPE -test $APP_SOURCE
+    popd
+}
+
+run_app() {
+    $SSH -t ${TEST_USER}@${TEST_STATION} "while killall ${TEST_APP}; do sleep 1; done" 2>&1 > /dev/null
+    $RSYNC -aHv --delete --exclude='.*' $BUILD_DIR/ ${TEST_USER}@${TEST_STATION}:${BUILD_DIR}
+    $SSH -t ${TEST_USER}@${TEST_STATION} "$TEST_ENV $BUILD_DIR/$TEST_APP $TEST_ARG"
+}
+
+if [ ! -x $RSYNC ]; then
+    echo "Please install rsync, first"
+    exit 1
+fi
+
+if [ ! -x $SSH ]; then
+    echo "Please install ssh, first (openssh-client)"
+    exit 1
+fi
+
+[ -x $CCBUILD ] || bootstrap_ccbuild
+build_app
+run_app
