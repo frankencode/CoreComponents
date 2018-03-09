69d64a174fd1b8f67a8e2a698c9b19ebde5a49cb
Frank Mertens <frank@cyblogic.de>
2018-03-09 04:27:47 +0100

    SDL support for querying display information

---
+++ ui/plugins/sdl/src/SdlDisplayManager.cc
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2017-2018 Frank Mertens.
+ *
+ * Distribution and use is allowed under the terms of the zlib license
+ * (see cc/LICENSE-zlib).
+ *
+ */
+
+#include <SDL2/SDL.h>
+#include <cc/Singleton>
+#include <cc/ui/SdlDisplayManager>
+
+namespace cc {
+namespace ui {
+
+SdlDisplayManager *SdlDisplayManager::instance()
+{
+    return Singleton<SdlDisplayManager>::instance();
+}
+
+Ref<Display> SdlDisplayManager::getDisplay(int index) const
+{
+    Ref<Display> display = Object::create<Display>();
+    {
+        float hdpi = 0;
+        float vdpi = 0;
+        if (SDL_GetDisplayDPI(index, NULL, &hdpi, &vdpi) != 0) {
+            // TODO...
+        }
+        display->dpi = Ratio{double(hdpi), double(vdpi)};
+    }
+    {
+        SDL_Rect rect;
+        if (SDL_GetDisplayBounds(index, &rect) != 0) {
+            // TODO...
+        }
+        display->pos = Point{ double(rect.x), double(rect.y) };
+        display->size = Size{ double(rect.w), double(rect.h) };
+    }
+    return display;
+}
+
+int SdlDisplayManager::displayCount() const
+{
+    return SDL_GetNumVideoDisplays();
+}
+
+}} // namespace cc::ui
--- ui/plugins/sdl/src/SdlPlatformPlugin.cc
+++ ui/plugins/sdl/src/SdlPlatformPlugin.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Frank Mertens.
+ * Copyright (C) 2017-2018 Frank Mertens.
  *
  * Distribution and use is allowed under the terms of the zlib license
  * (see cc/LICENSE-zlib).
@@ -9,6 +9,7 @@
 #include <cc/Registration>
 #include <cc/ui/SdlApplication>
 #include <cc/ui/SdlTimeMaster>
+#include <cc/ui/SdlDisplayManager>
 #include <cc/ui/SdlPlatformPlugin>
 
 namespace cc {
@@ -26,6 +27,11 @@ Application *SdlPlatformPlugin::application() const
 TimeMaster *SdlPlatformPlugin::timeMaster() const
 {
     return SdlTimeMaster::instance();
+}
+
+DisplayManager *SdlPlatformPlugin::displayManager() const
+{
+    return SdlDisplayManager::instance();
 }
 
 CC_REGISTRATION(SdlPlatformPlugin)
