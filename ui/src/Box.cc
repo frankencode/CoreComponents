4d80fea793ed0beff0c28b7876f7e876d9c2cc94
Frank Mertens <frank@cyblogic.de>
2018-03-29 03:29:17 +0200

    Removed definition

---
+++ ui/src/Box.cc
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2018 Frank Mertens.
+ *
+ * Distribution and use is allowed under the terms of the zlib license
+ * (see cc/LICENSE-zlib).
+ *
+ */
+
+#include <cc/ui/Box>
+
+namespace cc {
+namespace ui {
+
+}} // namespace cc::ui
