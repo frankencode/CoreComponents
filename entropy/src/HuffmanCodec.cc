1345118fa75326311be26b875f520c0a993b43f3
Frank Mertens <frank@cyblogic.de>
2018-05-29 15:24:46 +0200

    Modernization: improve overall formulation of the Huffman encoding algorithm... (WIP)

--- entropy/src/HuffmanCodec.cc
+++ entropy/src/HuffmanCodec.cc
@@ -22,56 +22,55 @@
 namespace cc {
 namespace entropy {
 
-HuffmanCodec::HuffmanCodec(int rawDiversity, int rawDynamicRange):
-    rawDiversity_(rawDiversity),
-    rawDynamicRange_(rawDynamicRange),
-    nodes_(Array<Node>::create(2 * rawDiversity)),
-    codeMap_(Array<NodeRef>::create(rawDynamicRange_)),
-    heap_(MaxHeap<NodeRef>::create(2 * rawDiversity)),
-    bitStack_(Stack<uint8_t>::create(rawDiversity))
+HuffmanCodec::HuffmanCodec():
+    buffer_(CharArray::allocate(4096)),
+    nodes_(Array<Node>::create(512)),
+    codeMap_(Array<NodeRef>::create(256)),
+    heap_(MinHeap<NodeRef>::create(512)),
+    bitStack_(Stack<uint8_t>::create(256))
 {}
 
 void HuffmanCodec::reset()
 {
-    fill_ = 0;
-    for (int i = 0; i < rawDynamicRange_; ++i)
-        codeMap_->at(i).node_ = 0;
+    nodesFill_ = 0;
+    for (NodeRef &entry: codeMap_)
+        entry.node_ = 0;
 }
 
 inline void HuffmanCodec::addSymbol(int value, int count0)
 {
-    nodes_->at(fill_) = Node {
+    nodes_->at(nodesFill_) = Node {
         .parent_ = 0,
         .leftChild_ = 0,
         .rightChild_ = 0,
         .value_ = value,
         .count_ = count0
     };
-    codeMap_->at(value).node_ = nodes_->data() + fill_;
-    ++fill_;
+    codeMap_->at(value).node_ = nodes_->data() + nodesFill_;
+    ++nodesFill_;
 }
 
 void HuffmanCodec::generateCodeTable()
 {
     heap_->deplete();
-    for (int i = 0; i < fill_; ++i)
+    for (int i = 0; i < nodesFill_; ++i)
         heap_->push(NodeRef{ .node_ = nodes_->data() + i});
 
     while (heap_->count() > 1)
     {
         NodeRef leftChild = heap_->pop();
         NodeRef rightChild = heap_->pop();
-        leftChild.node_->parent_ = nodes_->data() + fill_;
-        rightChild.node_->parent_ = nodes_->data() + fill_;
-        nodes_->at(fill_) = Node {
+        leftChild.node_->parent_ = nodes_->data() + nodesFill_;
+        rightChild.node_->parent_ = nodes_->data() + nodesFill_;
+        nodes_->at(nodesFill_) = Node {
             .parent_ = 0,
             .leftChild_ = leftChild.node_,
             .rightChild_ = rightChild.node_,
             .value_ = -1,
             .count_ = leftChild.node_->count_ + rightChild.node_->count_
         };
-        heap_->push(NodeRef{nodes_->data() + fill_});
-        ++fill_;
+        heap_->push(NodeRef{nodes_->data() + nodesFill_});
+        ++nodesFill_;
     }
 
     if (heap_->count() > 0)
@@ -80,151 +79,87 @@ void HuffmanCodec::generateCodeTable()
         root_ = nullptr;
 }
 
-void HuffmanCodec::writeRawFrame(BitSink *sink, int *raw, int rawFill, int rawMin, int rawMax)
+int HuffmanCodec::encode(Stream *source, BitSink *sink)
 {
-    /** write header
-      */
-    sink->writeUIntVlc(rawFill);
-    sink->writeIntVlc(rawMin);
-    sink->writeIntVlc(rawMax);
-    sink->writeBit(0);    // encoding flag
+    /// fill input buffer
 
-    const int bits = ilog2(rawMax - rawMin + 1);
-    for (int i = 0; i < rawFill; ++i)
-        sink->writeUInt(bits, raw[i] - rawMin);
-}
+    int bufferFill = source->read(mutate(buffer_));
 
-void HuffmanCodec::encode(BitSink *sink, int *raw, int rawFill, bool *userFallback)
-{
-    /** determine dynamic range (min, max)
-      */
-    int rawMin = raw[0];
-    int rawMax = raw[0];
+    if (bufferFill == 0) {
+        sink->writeUInt8(0);
+        return 0;
+    }
+
+    /// count how often each symbol appears in the input buffer
+
+    reset();
+    for (int i = 0; i < bufferFill; ++i) {
+        uint8_t value = buffer_->byteAt(i);
+        if (!codeMap_->at(value).node_) addSymbol(value, 1);
+        else ++codeMap_->at(value).node_->count_;
+    }
+
+    /// write header
+
+    sink->writeUInt8(nodesFill_);
+    sink->writeUInt16(bufferFill);
+
+    /// write symbol table with symbol counts
     {
-        for (int i = 1; i < rawFill; ++i)
+        heap_->deplete();
+        for (int i = 0; i < nodesFill_; ++i)
+            heap_->push(NodeRef{ .node_ = &nodes_->at(i) });
+
+        int maxCountDelta = 0;
         {
-            int x = raw[i];
-            if (x < rawMin) rawMin = x;
-            else if (x > rawMax) rawMax = x;
+            int previousCount = 0;
+
+            for (int i = 0; i < nodesFill_; ++i) {
+                NodeRef entry = heap_->pop();
+                int countDelta = entry.node_->count_ - previousCount;
+                if (countDelta > maxCountDelta) maxCountDelta = countDelta;
+                previousCount = entry.node_->count_;
+            }
+        }
+
+        int maxValue = 0;
+
+        for (int i = 0; i < nodesFill_; ++i) {
+            Node &node = nodes_->at(i);
+            if (node.value_ > maxValue) maxValue = node.value_;
+            heap_->push(NodeRef{ .node_ = &node });
+        }
+
+        int bitsPerSymbolValue = ilog<2>(maxValue);
+        int bitsPerCountDelta = ilog<2>(maxCountDelta);
+
+        sink->writeUInt<3>(bitsPerSymbolValue);
+        sink->writeUInt<5>(bitsPerCountDelta);
+
+        {
+            int previousCount = 0;
+
+            for (int i = 0; i < nodesFill_; ++i) {
+                NodeRef entry = heap_->pop();
+                int countDelta = entry.node_->count_ - previousCount;
+                sink->writeUInt(bitsPerSymbolValue, entry.node_->value_);
+                sink->writeUInt(bitsPerSymbolValue, countDelta);
+                previousCount = entry.node_->count_;
+            }
         }
     }
 
-    /** optional fallback to raw data transmission
-      */
-    if (rawMax - rawMin + 1 > rawDynamicRange_)
-    {
-#ifdef CC_ENTROPY_HUFFMANCODEC_PROFILING
-        CC_DEBUG <<
-            Format(
-                "Fallback to unencoded transmission, because out of dynamic range.\n"
-                "  (rawMax - rawMin + 1, rawDynamicRange_) = (%%, %%)"
-            ) << (rawMax - rawMin + 1) << rawDynamicRange_;
-#endif
-        if (!userFallback)
-            writeRawFrame(sink, raw, rawFill, rawMin, rawMax);
-        else
-            *userFallback = true;
-        return;
-    }
+    /// generate the Huffman code table
 
-    /** determine symbol frequencies
-      */
-    reset();
-    for (int i = 0; (i < rawFill) && (fill_ < rawDiversity_); ++i)
-    {
-        int x = raw[i] - rawMin;
-        if (!codeMap_->at(x).node_) addSymbol(x, 0);
-        ++codeMap_->at(x).node_->count_;
-    }
-
-    /** optional fallback to raw data transmission
-      */
-    if (fill_ == rawDiversity_)
-    {
-        if (!userFallback)
-            writeRawFrame(sink, raw, rawFill, rawMin, rawMax);
-        else
-            *userFallback = true;
-        return;
-    }
-
-    /** generate code table
-      */
-    int diversity = fill_;
     generateCodeTable();
 
-#ifdef CC_ENTROPY_HUFFMANCODEC_DEBUG_STATISTICS
-    for (int i = 0; i < diversity; ++i)
-        CC_DEBUG << i << ": H(" << nodes_->at(i).value << ") = " << nodes_->at(i).count;
-#endif
+    /// write Huffman encoded symbols
 
-    /** determine output size
-      */
-    int tableSize = 0;
-    int outSize = 1;    // encoding flag
-    outSize += BitSink::bitsPerUIntVlc(rawFill);
-    outSize += BitSink::bitsPerUIntVlc(rawMin);
-    tableSize += BitSink::bitsPerUIntVlc(diversity);
-    for (int i = 0; i < diversity; ++i)
+    for (int i = 0; i < bufferFill; ++i)
     {
-        tableSize += BitSink::bitsPerUIntVlc(nodes_->at(i).value_);
-        tableSize += BitSink::bitsPerUIntVlc(nodes_->at(i).count_);
-        int len = 0;
-        Node *node = nodes_->data() + i;
-        while ((node = node->parent_) != 0) ++len;
-        outSize += len * nodes_->at(i).count_;
-    }
+        Node *node = codeMap_->at(buffer_->byteAt(i)).node_;
 
-    outSize += tableSize;
-    int outSizeBytes = outSize / 8 + (outSize % 8 != 0);
-
-#ifdef CC_ENTROPY_HUFFMANCODEC_PROFILING
-    CC_DEBUG
-        << "diversity, outSize, table weight = "
-        << diversity << ", " <<  outSizeBytes << ", " << double(tableSize) / outSize;
-#endif // FTL_HUFFMANCODEC_PROFILING
-
-    /** optional fallback to raw data transmission
-      */
-    if (outSizeBytes > encodedCapacity(rawFill, rawMax - rawMin + 1))
-    {
-#ifdef CC_ENTROPY_HUFFMANCODEC_PROFILING
-        CC_DEBUG << "fallback to unencoded transmission";
-#endif
-        if (!userFallback)
-            writeRawFrame(sink, raw, rawFill, rawMin, rawMax);
-        else
-            *userFallback = true;
-        return;
-    }
-
-    if (userFallback) *userFallback = false;
-
-    /** write header
-      */
-    sink->writeUIntVlc(rawFill);
-    sink->writeIntVlc(rawMin);
-    sink->writeIntVlc(rawMax);
-    sink->writeBit(1);    // encoding flag
-
-    /** write frequency table
-      */
-    sink->writeUIntVlc(diversity);
-    for (int i = 0; i < diversity; ++i)
-    {
-        sink->writeUIntVlc(nodes_->at(i).value_);
-        sink->writeUIntVlc(nodes_->at(i).count_);
-    }
-
-    /** encode symbols
-      */
-    for (int i = 0; i < rawFill; ++i)
-    {
-        int x = raw[i] - rawMin;
-
-        Node *node = codeMap_->at(x).node_;
         bitStack_->deplete();
-
         while (node->parent_) {
             Node *parent = node->parent_;
             bitStack_->push(parent->rightChild_ == node);
@@ -234,68 +169,50 @@ void HuffmanCodec::encode(BitSink *sink, int *raw, int rawFill, bool *userFallba
         while (bitStack_->count() > 0)
             sink->writeBit(bitStack_->pop());
     }
+
+    return bufferFill;
 }
 
-int HuffmanCodec::decode(int *raw, int rawCapacity, BitSource *source)
+int HuffmanCodec::decode(BitSource *source, Stream *sink)
 {
-    /** read header
-      */
-    int rawFill = source ->readUIntVlc();
-    int rawMin = source->readIntVlc();
-    int rawMax = source->readIntVlc();
-    int encodingFlag = source->readBit();
+    /// read header
 
-    if (rawFill > rawCapacity) throw BufferOverflow();
+    int symbolCount = source->readUInt8();
 
-    /** optional fallback to raw data transmission
-      */
-    if (encodingFlag == 0)
+    if (symbolCount == 0) return 0;
+
+    int bufferFill = source->readUInt16();
+
+    /// read symbol table
     {
-        const int bits = ilog2(rawMax - rawMin + 1);
-        for (int i = 0; i < rawFill; ++i)
-            raw[i] = int(source->readUInt(bits)) + rawMin;
-        return rawFill;
+        int bitsPerSymbolValue = source->readUInt<3>();
+        int bitsPerCountDelta = source->readUInt<5>();
+
+        reset();
+        int count = 0;
+        for (int i = 0; i < symbolCount; ++i) {
+            int value = source->readUInt(bitsPerSymbolValue);
+            count += source->readUInt(bitsPerCountDelta);
+            addSymbol(value, count);
+        }
     }
 
-    /** read frequency table
-      */
-    reset();
-    int diversity = source ->readUIntVlc();
-    for (int i = 0; i < diversity; ++i)
-    {
-        int value = source->readUIntVlc();
-        int count = source->readUIntVlc();
-        addSymbol(value, count);
-    }
+    /// generate Huffman code table
 
-    /** generate code table
-      */
     generateCodeTable();
 
-    /** decode symbols
-      */
-    for (int i = 0; i < rawFill; ++i)
-    {
+    /// decode Huffman encoded symbols
+
+    for (int i = 0; i < bufferFill; ++i) {
         Node *node = root_;
         while (node->leftChild_)
-            node = (source->readBit()) ?  node->rightChild_ : node->leftChild_;
-        raw[i] = node->value_ + rawMin;
+            node = (source->readBit()) ? node->rightChild_ : node->leftChild_;
+        mutate(buffer_)->at(i) = node->value_;
     }
 
-    return rawFill;
-}
+    sink->write(buffer_->select(bufferFill));
 
-int HuffmanCodec::encodedCapacity(int rawCapacity, int rawDynamicRange) const
-{
-    int bytesPerSymbol = 1;
-    bytesPerSymbol += ((rawDynamicRange >> 8) > 1);
-    bytesPerSymbol += ((rawDynamicRange >> 16) > 1);
-    bytesPerSymbol += ((rawDynamicRange >> 24) > 1);
-
-    int h = 36 * rawCapacity * bytesPerSymbol;
-    h = h / 32 + (h % 32 != 0);
-    h += 32;    // max frame header size
-    return h;
+    return bufferFill;
 }
 
 }} // namespace cc::entropy
