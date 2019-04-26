9b588a31fd2245bd71195c98240a7e4b7059e890
Frank Mertens <frank@cyblogic.de>
2019-04-26 07:44:25 +0200

    Added additional test cases for HMAC-SHA1.

--- crypto/tests/testHashMacSink.cc
+++ crypto/tests/testHashMacSink.cc
@@ -10,6 +10,7 @@
 #include <cc/stdio>
 #include <cc/debug>
 #include <cc/crypto/Md5Sink>
+#include <cc/crypto/Sha1Sink>
 #include <cc/crypto/HashMacSink>
 
 using namespace cc;
@@ -52,9 +53,46 @@ class SimpleMd5MacTest: public TestCase
     }
 };
 
+class SimpleSha1MacTest: public TestCase
+{
+    void run()
+    {
+        auto sha1Sink = Sha1Sink::open();
+
+        {
+            String key = String::allocate(20);;
+            for (int i = 0; i < key->count(); ++i)
+                mutate(key)->byteAt(i) = 0x0B;
+
+            String text = "Hi There";
+
+            auto macSink = HashMacSink::open(sha1Sink, key);
+            macSink->write(text);
+            String mac = macSink->finish()->toHex();
+
+            fout() << "HMAC-SHA1_0 = 0x" << mac << nl;
+
+            CC_VERIFY(mac == "b617318655057264e28bc0b6fb378c8ef146be00");
+        }
+        {
+            String key = "Jefe";
+            String text = "what do ya want for nothing?";
+
+            auto macSink = HashMacSink::open(sha1Sink, key);
+            macSink->write(text);
+            String mac = macSink->finish()->toHex();
+
+            fout() << "HMAC-SHA1_1 = 0x" << mac << nl;
+
+            CC_VERIFY(mac == "effcdf6ae5eb2fa2d27416d5f184df9c259a7c79");
+        }
+    }
+};
+
 int main(int argc, char **argv)
 {
     CC_TESTSUITE_ADD(SimpleMd5MacTest);
+    CC_TESTSUITE_ADD(SimpleSha1MacTest);
 
     return TestSuite::instance()->run(argc, argv);
 }
