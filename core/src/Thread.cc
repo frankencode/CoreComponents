71af918b9b95f2dfc3320ab531e4da23fa6817f9
Frank Mertens <frank@cyblogic.de>
2019-08-08 01:27:52 +0200

    Make sure the stack size is never set to a to small value  (e.g. PTHREAD_STACK_MIN=1<<17 on AARCH64)

--- core/src/Thread.cc
+++ core/src/Thread.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2017 Frank Mertens.
+ * Copyright (C) 2007-2019 Frank Mertens.
  *
  * Distribution and use is allowed under the terms of the zlib license
  * (see cc/LICENSE-zlib).
@@ -9,6 +9,7 @@
 #include <sys/mman.h>
 #include <time.h>
 #include <math.h>
+#include <limits.h>
 #include <cc/strings>
 #include <cc/exceptions>
 #include <cc/System>
@@ -25,34 +26,6 @@ Thread *Thread::self()
         self_->tid_ = pthread_self();
     }
     return self_;
-}
-
-class ThreadStack: public CharArray
-{
-public:
-    ThreadStack(char *data, int size):
-        CharArray(data, size, freeStack)
-    {}
-
-    static void freeStack(CharArray *stack) {
-        if (::munmap(stack->bytes(), stack->count()) == -1)
-            CC_SYSTEM_DEBUG_ERROR(errno);
-    }
-};
-
-Ref<CharArray> Thread::allocateStack(int stackSize, int guardSize)
-{
-    #ifndef MAP_ANONYMOUS
-    #define MAP_ANONYMOUS MAP_ANON
-    #endif
-    #ifndef MAP_GROWSDOWN
-    #define MAP_GROWSDOWN 0
-    #endif
-    void *protection = ::mmap(0, stackSize, PROT_NONE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
-    if (protection == MAP_FAILED) CC_SYSTEM_DEBUG_ERROR(errno);
-    void *stack = ::mmap((char *)protection + guardSize, stackSize - 2 * guardSize, PROT_READ|PROT_WRITE, MAP_FIXED|MAP_PRIVATE|MAP_ANONYMOUS|MAP_GROWSDOWN, -1, 0);
-    if (stack == MAP_FAILED) CC_SYSTEM_DEBUG_ERROR(errno);
-    return new ThreadStack((char *)protection, stackSize);
 }
 
 void *Thread::bootstrap(void *self)
@@ -73,18 +46,10 @@ Thread *Thread::start()
     }
 
     if (stackSize_ > 0) {
+        if (stackSize_ < PTHREAD_STACK_MIN) stackSize_ = PTHREAD_STACK_MIN;
         int ret = pthread_attr_setstacksize(&attr, stackSize_);
         if (ret != 0) CC_SYSTEM_DEBUG_ERROR(ret);
     }
-
-    #if 0
-    { // setting a custom stack became disfunctional with some glibc versions
-        const int guardSize = System::pageSize();
-        stack_ = allocateStack(stackSize_, guardSize);
-        int ret = pthread_attr_setstack(&attr, stack_->bytes() + guardSize, stack_->count() - 2 * guardSize);
-        if (ret != 0) CC_SYSTEM_DEBUG_ERROR(ret);
-    }
-    #endif
 
     int ret = pthread_create(&tid_, &attr, &bootstrap, static_cast<void *>(this));
     if (ret != 0) CC_SYSTEM_DEBUG_ERROR(ret);
