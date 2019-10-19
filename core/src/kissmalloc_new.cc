867dea49f9daf7ce65cb816fa86f75385ec0d43d
Frank Mertens <frank@cyblogic.de>
2019-10-19 21:54:37 +0200

    Added new/delete operator overloads up to C++17

--- core/src/kissmalloc_new.cc
+++ core/src/kissmalloc_new.cc
@@ -27,7 +27,17 @@
 #endif
 #endif
 
-void *operator new(std::size_t size)
+#if __cplusplus <= 199711L // until C++17
+#define KISSMALLOC_NOEXCEPT throw()
+#define KISSMALLOC_EXCEPT throw(std::bad_alloc)
+#define KISSMALLOC_THROW throw std::bad_alloc()
+#else // since C++17
+#define KISSMALLOC_NOEXCEPT noexcept
+#define KISSMALLOC_EXCEPT
+#define KISSMALLOC_THROW throw std::bad_alloc{}
+#endif // until/since C++17
+
+void *operator new(std::size_t size) KISSMALLOC_EXCEPT
 {
     #ifndef KISSMALLOC_VALGRIND
     void *data = KISSMALLOC_NAME(malloc)(size);
@@ -39,11 +49,11 @@ void *operator new(std::size_t size)
     VALGRIND_MALLOCLIKE_BLOCK(data, size, KISSMALLOC_REDZONE_SIZE, /*is_zeroed=*/true);
     #endif
     #endif
-    if (!data) throw std::bad_alloc{};
+    if (!data) KISSMALLOC_THROW;
     return data;
 }
 
-void *operator new[](std::size_t size)
+void *operator new[](std::size_t size) KISSMALLOC_EXCEPT
 {
     #ifndef KISSMALLOC_VALGRIND
     void *data = KISSMALLOC_NAME(malloc)(size);
@@ -55,39 +65,11 @@ void *operator new[](std::size_t size)
     VALGRIND_MALLOCLIKE_BLOCK(data, size, KISSMALLOC_REDZONE_SIZE, /*is_zeroed=*/true);
     #endif
     #endif
-    if (!data) throw std::bad_alloc{};
+    if (!data) KISSMALLOC_THROW;
     return data;
 }
 
-void operator delete(void *data) noexcept
-{
-    #ifndef KISSMALLOC_VALGRIND
-    KISSMALLOC_NAME(free)(data);
-    #else
-    #ifdef KISSMALLOC_OVERLOAD_LIBC
-    free(data);
-    #else
-    KISSMALLOC_NAME(free)((void *)((char *)data - KISSMALLOC_REDZONE_SIZE));
-    VALGRIND_FREELIKE_BLOCK(data, KISSMALLOC_REDZONE_SIZE);
-    #endif
-    #endif
-}
-
-void operator delete[](void *data) noexcept
-{
-    #ifndef KISSMALLOC_VALGRIND
-    KISSMALLOC_NAME(free)(data);
-    #else
-    #ifdef KISSMALLOC_OVERLOAD_LIBC
-    free(data);
-    #else
-    KISSMALLOC_NAME(free)((void *)((char *)data - KISSMALLOC_REDZONE_SIZE));
-    VALGRIND_FREELIKE_BLOCK(data, KISSMALLOC_REDZONE_SIZE);
-    #endif
-    #endif
-}
-
-void *operator new(std::size_t size, const std::nothrow_t &) noexcept
+void *operator new(std::size_t size, const std::nothrow_t &) KISSMALLOC_NOEXCEPT
 {
     #ifndef KISSMALLOC_VALGRIND
     void *data = KISSMALLOC_NAME(malloc)(size);
@@ -102,7 +84,7 @@ void *operator new(std::size_t size, const std::nothrow_t &) noexcept
     return data;
 }
 
-void *operator new[](std::size_t size, const std::nothrow_t &) noexcept
+void *operator new[](std::size_t size, const std::nothrow_t &) KISSMALLOC_NOEXCEPT
 {
     #ifndef KISSMALLOC_VALGRIND
     void *data = KISSMALLOC_NAME(malloc)(size);
@@ -117,7 +99,85 @@ void *operator new[](std::size_t size, const std::nothrow_t &) noexcept
     return data;
 }
 
-void operator delete(void *data, const std::nothrow_t &) noexcept
+#if __cplusplus >= 201703L // since C++17
+
+void *operator new(std::size_t size, std::align_val_t alignment)
+{
+    void *data = nullptr;
+    #ifndef KISSMALLOC_VALGRIND
+    if (KISSMALLOC_NAME(posix_memalign)(&data, alignment, size) != 0) KISSMALLOC_THROW;
+    #else
+    #ifdef KISSMALLOC_OVERLOAD_LIBC
+    if (KISSMALLOC_NAME(posix_memalign)(&data, alignment, size) != 0) KISSMALLOC_THROW;
+    #else
+    if (KISSMALLOC_NAME(posix_memalign)(&data, alignment, size + 2 * KISSMALLOC_REDZONE_SIZE) != 0) KISSMALLOC_THROW;
+    if (data) {
+        data = (void *)((char *)data + KISSMALLOC_REDZONE_SIZE);
+        VALGRIND_MALLOCLIKE_BLOCK(data, size, KISSMALLOC_REDZONE_SIZE, /*is_zeroed=*/true);
+    }
+    #endif
+    #endif
+}
+
+void *operator new[](std::size_t size, std::align_val_t alignment)
+{
+    void *data = nullptr;
+    #ifndef KISSMALLOC_VALGRIND
+    if (KISSMALLOC_NAME(posix_memalign)(&data, alignment, size) != 0) KISSMALLOC_THROW;
+    #else
+    #ifdef KISSMALLOC_OVERLOAD_LIBC
+    if (KISSMALLOC_NAME(posix_memalign)(&data, alignment, size) != 0) KISSMALLOC_THROW;
+    #else
+    if (KISSMALLOC_NAME(posix_memalign)(&data, alignment, size + 2 * KISSMALLOC_REDZONE_SIZE) != 0) KISSMALLOC_THROW;
+    if (data) {
+        data = (void *)((char *)data + KISSMALLOC_REDZONE_SIZE);
+        VALGRIND_MALLOCLIKE_BLOCK(data, size, KISSMALLOC_REDZONE_SIZE, /*is_zeroed=*/true);
+    }
+    #endif
+    #endif
+}
+
+void *operator new(std::size_t size, std::align_val_t alignment, const std::nothrow_t &)
+{
+    void *data = nullptr;
+    #ifndef KISSMALLOC_VALGRIND
+    KISSMALLOC_NAME(posix_memalign)(&data, alignment, size);
+    #else
+    #ifdef KISSMALLOC_OVERLOAD_LIBC
+    KISSMALLOC_NAME(posix_memalign)(&data, alignment, size);
+    #else
+    KISSMALLOC_NAME(posix_memalign)(&data, alignment, size + 2 * KISSMALLOC_REDZONE_SIZE);
+    if (data) {
+        data = (void *)((char *)data + KISSMALLOC_REDZONE_SIZE);
+        VALGRIND_MALLOCLIKE_BLOCK(data, size, KISSMALLOC_REDZONE_SIZE, /*is_zeroed=*/true);
+    }
+    #endif
+    #endif
+    return data;
+}
+
+void *operator new[](std::size_t size, std::align_val_t alignment, const std::nothrow_t &)
+{
+    void *data = nullptr;
+    #ifndef KISSMALLOC_VALGRIND
+    KISSMALLOC_NAME(posix_memalign)(&data, alignment, size);
+    #else
+    #ifdef KISSMALLOC_OVERLOAD_LIBC
+    KISSMALLOC_NAME(posix_memalign)(&data, alignment, size);
+    #else
+    KISSMALLOC_NAME(posix_memalign)(&data, alignment, size + 2 * KISSMALLOC_REDZONE_SIZE);
+    if (data) {
+        data = (void *)((char *)data + KISSMALLOC_REDZONE_SIZE);
+        VALGRIND_MALLOCLIKE_BLOCK(data, size, KISSMALLOC_REDZONE_SIZE, /*is_zeroed=*/true);
+    }
+    #endif
+    #endif
+    return data;
+}
+
+#endif // since C++17
+
+void operator delete(void *data) KISSMALLOC_NOEXCEPT
 {
     #ifndef KISSMALLOC_VALGRIND
     KISSMALLOC_NAME(free)(data);
@@ -131,7 +191,7 @@ void operator delete(void *data, const std::nothrow_t &) noexcept
     #endif
 }
 
-void operator delete[](void *data, const std::nothrow_t &) noexcept
+void operator delete[](void *data) KISSMALLOC_NOEXCEPT
 {
     #ifndef KISSMALLOC_VALGRIND
     KISSMALLOC_NAME(free)(data);
@@ -144,3 +204,123 @@ void operator delete[](void *data, const std::nothrow_t &) noexcept
     #endif
     #endif
 }
+
+void operator delete(void *data, const std::nothrow_t &) KISSMALLOC_NOEXCEPT
+{
+    #ifndef KISSMALLOC_VALGRIND
+    KISSMALLOC_NAME(free)(data);
+    #else
+    #ifdef KISSMALLOC_OVERLOAD_LIBC
+    free(data);
+    #else
+    KISSMALLOC_NAME(free)((void *)((char *)data - KISSMALLOC_REDZONE_SIZE));
+    VALGRIND_FREELIKE_BLOCK(data, KISSMALLOC_REDZONE_SIZE);
+    #endif
+    #endif
+}
+
+void operator delete[](void *data, const std::nothrow_t &) KISSMALLOC_NOEXCEPT
+{
+    #ifndef KISSMALLOC_VALGRIND
+    KISSMALLOC_NAME(free)(data);
+    #else
+    #ifdef KISSMALLOC_OVERLOAD_LIBC
+    free(data);
+    #else
+    KISSMALLOC_NAME(free)((void *)((char *)data - KISSMALLOC_REDZONE_SIZE));
+    VALGRIND_FREELIKE_BLOCK(data, KISSMALLOC_REDZONE_SIZE);
+    #endif
+    #endif
+}
+
+#if __cplusplus >= 201402L // since C++14
+
+void operator delete(void *data, std::size_t size) noexcept
+{
+    #ifndef KISSMALLOC_VALGRIND
+    KISSMALLOC_NAME(free)(data);
+    #else
+    #ifdef KISSMALLOC_OVERLOAD_LIBC
+    free(data);
+    #else
+    KISSMALLOC_NAME(free)((void *)((char *)data - KISSMALLOC_REDZONE_SIZE));
+    VALGRIND_FREELIKE_BLOCK(data, KISSMALLOC_REDZONE_SIZE);
+    #endif
+    #endif
+}
+
+void operator delete[](void *data, std::size_t size) noexcept
+{
+    #ifndef KISSMALLOC_VALGRIND
+    KISSMALLOC_NAME(free)(data);
+    #else
+    #ifdef KISSMALLOC_OVERLOAD_LIBC
+    free(data);
+    #else
+    KISSMALLOC_NAME(free)((void *)((char *)data - KISSMALLOC_REDZONE_SIZE));
+    VALGRIND_FREELIKE_BLOCK(data, KISSMALLOC_REDZONE_SIZE);
+    #endif
+    #endif
+}
+
+#endif // since C++14
+
+#if __cplusplus >= 201703L // since C++17
+
+void operator delete(void *data, std::align_val_t alignment) noexcept
+{
+    #ifndef KISSMALLOC_VALGRIND
+    KISSMALLOC_NAME(free)(data);
+    #else
+    #ifdef KISSMALLOC_OVERLOAD_LIBC
+    free(data);
+    #else
+    KISSMALLOC_NAME(free)((void *)((char *)data - KISSMALLOC_REDZONE_SIZE));
+    VALGRIND_FREELIKE_BLOCK(data, KISSMALLOC_REDZONE_SIZE);
+    #endif
+    #endif
+}
+
+void operator delete[](void *data, std::align_val_t alignment) noexcept
+{
+    #ifndef KISSMALLOC_VALGRIND
+    KISSMALLOC_NAME(free)(data);
+    #else
+    #ifdef KISSMALLOC_OVERLOAD_LIBC
+    free(data);
+    #else
+    KISSMALLOC_NAME(free)((void *)((char *)data - KISSMALLOC_REDZONE_SIZE));
+    VALGRIND_FREELIKE_BLOCK(data, KISSMALLOC_REDZONE_SIZE);
+    #endif
+    #endif
+}
+
+void operator delete(void *data, std::size_t size, std::align_val_t alignment) noexcept
+{
+    #ifndef KISSMALLOC_VALGRIND
+    KISSMALLOC_NAME(free)(data);
+    #else
+    #ifdef KISSMALLOC_OVERLOAD_LIBC
+    free(data);
+    #else
+    KISSMALLOC_NAME(free)((void *)((char *)data - KISSMALLOC_REDZONE_SIZE));
+    VALGRIND_FREELIKE_BLOCK(data, KISSMALLOC_REDZONE_SIZE);
+    #endif
+    #endif
+}
+
+void operator delete[](void *data, std::size_t size, std::align_val_t alignment) noexcept
+{
+    #ifndef KISSMALLOC_VALGRIND
+    KISSMALLOC_NAME(free)(data);
+    #else
+    #ifdef KISSMALLOC_OVERLOAD_LIBC
+    free(data);
+    #else
+    KISSMALLOC_NAME(free)((void *)((char *)data - KISSMALLOC_REDZONE_SIZE));
+    VALGRIND_FREELIKE_BLOCK(data, KISSMALLOC_REDZONE_SIZE);
+    #endif
+    #endif
+}
+
+#endif // since C++17
