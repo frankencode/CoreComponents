8a6a9eb2133e161989a081dd761a06fa13c9fc33
Frank Mertens <frank@cyblogic.de>
2019-11-19 06:43:38 +0100

    Code refactoring and modernisation

--- core/src/OrdinalTree.cc
+++ core/src/OrdinalTree.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2017 Frank Mertens.
+ * Copyright (C) 2007-2019 Frank Mertens.
  *
  * Distribution and use is allowed under the terms of the zlib license
  * (see cc/LICENSE-zlib).
@@ -9,171 +9,6 @@
 #include <cc/OrdinalTree>
 
 namespace cc {
-
-void AvlBalance::restore(OrdinalTree *tree, OrdinalNode *kp, bool left, bool attached)
-{
-    if (!kp) return;
-
-    int delta = 2 * left - 1;
-    OrdinalNode *k = kp;
-    while (true) {
-        k->balance_ += attached ? delta : -delta;
-        if ((k->balance_ == 2) || (k->balance_ == -2))
-            k = rebalance(tree, k);
-        if ( (attached ? (k->balance_ == 0) : (k->balance_ != 0)) ||
-             (!k->parent_) )
-            break;
-        delta = 2 * (k->parent_->left_ == k) - 1;
-        k = k->parent_;
-    }
-}
-
-OrdinalNode *AvlBalance::rebalance(OrdinalTree *tree, OrdinalNode *k1)
-{
-    if (k1->balance_ == 2) {
-        OrdinalNode *k2 = k1->left_;
-        if (k2->balance_ == -1) {
-            // -- case Ia->III
-            OrdinalNode *k3 = k2->right_;
-            k1->balance_ = -(k3->balance_ == 1);
-            k2->balance_ = (k3->balance_ == -1);
-            k3->balance_ = 0;
-            tree->rotate(k2, true);
-            tree->rotate(k1, false);
-        }
-        else {
-            // -- case Ib, Ic
-            k1->balance_ = (k2->balance_ <= 0);
-            k2->balance_ = k2->balance_ - 1;
-            tree->rotate(k1, false);
-        }
-    }
-    else {
-        OrdinalNode *k2 = k1->right_;
-        if (k2->balance_ == 1) {
-            // -- case IIc->IV
-            OrdinalNode *k3 = k2->left_;
-            k1->balance_ = (k3->balance_ == -1);
-            k2->balance_ = -(k3->balance_ == 1);
-            k3->balance_ = 0;
-            tree->rotate(k2, false);
-            tree->rotate(k1, true);
-        }
-        else {
-            // -- case IIa, IIb
-            k1->balance_ = -(k2->balance_ >= 0);
-            k2->balance_ = k2->balance_ + 1;
-            tree->rotate(k1, true);
-        }
-    }
-    return k1->parent_;
-}
-
-#ifndef NDEBUG
-
-int AvlBalance::height(OrdinalNode *k)
-{
-    if (k == nullptr) return 0;
-    int h1 = height(k->left_);
-    int h2 = height(k->right_);
-    return (h1 < h2 ? h2 : h1) + 1;
-}
-
-bool AvlBalance::testBalance(OrdinalNode *k)
-{
-    if (!k) return true;
-    if (!((k->balance_ == -1) || (k->balance_ == 0) || (k->balance_ == 1))) return false;
-    if ((height(k->left_) - height(k->right_)) != k->balance_) return false;
-    return testBalance(k->left_) && testBalance(k->right_);
-}
-
-#endif // ndef NDEBUG
-
-/** Attaches the new leaf node k under kp as the left or right child.
-  */
-void OrdinalTree::attach(OrdinalNode *kp, OrdinalNode *k, bool left)
-{
-    if (kp) {
-        if (left)
-            kp->left_ = k;
-        else
-            kp->right_ = k;
-        k->parent_ = kp;
-        k->left_ = nullptr;
-        k->right_ = nullptr;
-    }
-    else {
-        root_ = k;
-        k->parent_ = nullptr;
-        k->left_ = nullptr;
-        k->right_ = nullptr;
-    }
-    changed(kp, k, left, true);
-}
-
-/** Detaches the leaf or list node k from the tree.
-  */
-OrdinalNode *OrdinalTree::detach(OrdinalNode *k)
-{
-    OrdinalNode *kp = k->parent_;
-    bool left = false;
-    if (kp) {
-        OrdinalNode *kc = (k->left_ != nullptr) ? k->left_ : k->right_;
-        left = (kp->left_ == k);
-        if (left)
-            kp->left_ = kc;
-        else
-            kp->right_ = kc;
-        if (kc != nullptr) kc->parent_ = kp;
-    }
-    else {
-        root_ = nullptr;
-    }
-    changed(kp, k, left, false);
-    return k;
-}
-
-/** Put the leaf node kl into the place of inner node ki
-  */
-void OrdinalTree::replace(OrdinalNode *ki, OrdinalNode *kl)
-{
-    // establish links from to kl to the neighbors of ki (and take over weight and balance, but not value of ki)
-    *kl = *ki;
-
-    // establish links from neighbors to kl
-    OrdinalNode *kp = ki->parent_;
-    if (kp) {
-        if (kp->left_ == ki)
-            kp->left_ = kl;
-        else
-            kp->right_ = kl;
-    }
-    else {
-        root_ = kl;
-    }
-    if (ki->left_)
-        ki->left_->parent_ = kl;
-    if (ki->right_)
-        ki->right_->parent_ = kl;
-}
-
-/** Remove the node k from the tree.
-  */
-OrdinalNode *OrdinalTree::unlink(OrdinalNode *k)
-{
-    if (!k->left_ && !k->right_)
-        detach(k);
-    else if (!k->left_)
-        replace(k, detach(min(k->right_)));
-    else if (!k->right_)
-        replace(k, detach(max(k->left_)));
-    else if (k->left_->weight_ < k->right_->weight_)
-        replace(k, detach(min(k->right_)));
-    else
-        replace(k, detach(max(k->left_)));
-
-    return k;
-}
 
 int OrdinalTree::getIndexOf(OrdinalNode *k)
 {
@@ -186,99 +21,6 @@ int OrdinalTree::getIndexOf(OrdinalNode *k)
         k = kp;
     }
     return i;
-}
-
-OrdinalNode *OrdinalTree::pred(OrdinalNode *k)
-{
-    if (k->left_)
-        return max(k->left_);
-    OrdinalNode *kp = k->parent_;
-    while (kp) {
-        if (k == kp->right_) break;
-        k = kp;
-        kp = kp->parent_;
-    }
-    return kp;
-}
-
-OrdinalNode *OrdinalTree::succ(OrdinalNode *k)
-{
-    if (k->right_)
-        return min(k->right_);
-    OrdinalNode *kp = k->parent_;
-    while (kp) {
-        if (k == kp->left_) break;
-        k = kp;
-        kp = kp->parent_;
-    }
-    return kp;
-}
-
-OrdinalNode *OrdinalTree::min(OrdinalNode *k)
-{
-    OrdinalNode *k2 = k;
-    while (k) {
-        k2 = k;
-        k = k->left_;
-    }
-    return k2;
-}
-
-OrdinalNode *OrdinalTree::max(OrdinalNode *k)
-{
-    OrdinalNode *k2 = k;
-    while (k) {
-        k2 = k;
-        k = k->right_;
-    }
-    return k2;
-}
-
-/** Perform an equivalent tree transformation.
-  *
-  * Rotate left:
-  *
-  *    k1      =>      k2
-  *   /  \            /  \
-  *  a    k2         k1   c
-  *      /  \       /  \
-  *     b    c     a    b
-  *
-  * Rotate right:
-  *
-  *      k1    =>    k2
-  *     /  \        /  \
-  *    k2   c      a    k1
-  *   /  \             /  \
-  *  a    b           b    c
-  */
-void OrdinalTree::rotate(OrdinalNode *k1, bool left)
-{
-    OrdinalNode *k2 = left ? k1->right_ : k1->left_;
-    if (k1->parent_) {
-        if (k1->parent_->left_ == k1)
-            k1->parent_->left_ = k2;
-        else
-            k1->parent_->right_ = k2;
-    }
-    else {
-        root_ = k2;
-    }
-    k2->parent_ = k1->parent_;
-    k1->parent_ = k2;
-    if (left) {
-        k1->right_ = k2->left_;
-        if (k2->left_)
-            k2->left_->parent_ = k1;
-        k2->left_ = k1;
-    }
-    else {
-        k1->left_ = k2->right_;
-        if (k2->right_)
-            k2->right_->parent_ = k1;
-        k2->right_ = k1;
-    }
-    rotated(k1, left);
 }
 
 void OrdinalTree::populate(OrdinalNode **v, int n)
@@ -302,7 +44,7 @@ void OrdinalTree::populate(OrdinalNode **v, int n)
                 k->right_ = nullptr;
             }
             else {
-                AvlBalance::restore(this, v[(i - 1) >> 1], i & 1, false);
+                restoreBalance(v[(i - 1) >> 1], i & 1, false);
             }
         }
     }
@@ -436,7 +178,7 @@ void OrdinalTree::changed(OrdinalNode *kp, OrdinalNode *kc, bool left, bool atta
         k = k->parent_;
     }
 
-    AvlBalance::restore(this, kp, left, attached);
+    restoreBalance(kp, left, attached);
 
     cachedNode_ = nullptr;
 }
@@ -449,39 +191,12 @@ void OrdinalTree::rotated(OrdinalNode *k1, bool /*left*/)
 
 #ifndef NDEBUG
 
-bool OrdinalTree::testStructure(OrdinalNode *k)
-{
-    if (!k) return true;
-    if (k->parent_) {
-        if (!((k == k->parent_->left_) || (k == k->parent_->right_)))
-            return false;
-    }
-    return testStructure(k->left_) && testStructure(k->right_);
-}
-
 bool OrdinalTree::testWeight(OrdinalNode *k)
 {
     if (!k) return true;
     return
         (weight(k->left_) + weight(k->right_) + 1 == k->weight_) &&
         testWeight(k->left_) && testWeight(k->right_);
-}
-
-bool OrdinalTree::testIteration(OrdinalNode *k)
-{
-    if (k == 0) return true;
-    OrdinalNode *k2;
-    k2 = succ(k);
-    if (k2) {
-        if (k != pred(k2))
-            return false;
-    }
-    k2 = pred(k);
-    if (k2) {
-        if (k != succ(k2))
-            return false;
-    }
-    return testIteration(k->left_) && testIteration(k->right_);
 }
 
 #endif // ndef NDEBUG
