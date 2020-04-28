51e16afb37a585ad3b700e076fad12dba78c4d19
Frank Mertens <frank@cyblogic.de>
2020-04-28 17:38:51 +0200

    Make ccget send UA string

--- node/tools/get/main.cc
+++ node/tools/get/main.cc
@@ -3,7 +3,7 @@
 #include <cc/node/HttpRequestGenerator>
 #include <cc/net/Uri>
 #include <cc/stdio>
-#include <cc/Arguments>
+#include <cc/KernelInfo>
 
 using namespace cc;
 using namespace cc::net;
@@ -14,36 +14,46 @@ int main(int argc, char **argv)
     auto toolName = String{argv[0]}->fileName();
 
     try {
-        auto arguments = Arguments::parse(argc, argv, VariantMap::create());
-        auto items = arguments->items();
+        if (argc < 2) throw HelpRequest{};
 
-        for (auto item: items) {
-            auto security = HttpClientSecurity::createDefault();
-            auto uri = Uri::parse(item);
-            if (uri->port() <= 0) uri->setPort(uri->scheme() == "https" ? 443 : 80);
-            if (uri->path() == "") uri->setPath("/");
-            if (uri->scheme() == "http") security = nullptr;
-            auto address = SocketAddress::resolve(uri);
-            auto socket = HttpClientSocket::connect(address, uri->host(), security);
-            auto connection = HttpClientConnection::open(socket);
-            auto request = HttpRequestGenerator::create(connection);
-            request->setMethod("GET");
-            request->setHost(uri->requestHost());
-            request->setPath(uri->requestPath());
-            request->transmit();
-            auto response = connection->readResponse();
-            if (response->statusCode() == 200) {
-                response->payload()->transferTo(stdOut());
-            }
-            else {
-                ferr() << response->version() << " " << response->statusCode() << " " << response->reasonPhrase() << nl;
-            }
+        String commandLine;
+        {
+            auto parts = StringList::create();
+            for (int i = 1; i < argc; ++i)
+                parts->append(argv[i]);
+            commandLine = parts->join();
+            commandLine = commandLine->replace(" ", "%20");
+        }
+
+        auto security = HttpClientSecurity::createDefault();
+        auto uri = Uri::parse(commandLine);
+        if (uri->port() <= 0) uri->setPort(uri->scheme() == "https" ? 443 : 80);
+        if (uri->path() == "") uri->setPath("/");
+        if (uri->scheme() == "http") security = nullptr;
+        auto address = SocketAddress::resolve(uri);
+        auto socket = HttpClientSocket::connect(address, uri->host(), security);
+        auto connection = HttpClientConnection::open(socket);
+        auto request = HttpRequestGenerator::create(connection);
+        request->setMethod("GET");
+        request->setHost(uri->requestHost());
+        request->setPath(uri->requestPath());
+        {
+            auto kernelInfo = KernelInfo::query();
+            request->setHeader("User-Agent", Format{"ccnode 1.0 (%%; %%)"} << kernelInfo->name() << kernelInfo->machine());
+        }
+        request->transmit();
+        auto response = connection->readResponse();
+        if (response->statusCode() == 200) {
+            response->payload()->transferTo(stdOut());
+        }
+        else {
+            ferr() << response->version() << " " << response->statusCode() << " " << response->reasonPhrase() << nl;
         }
     }
-    catch (HelpRequest &ex) {
+    catch (HelpRequest &) {
         fout(
-            "Usage: %% [URI]\n"
-            "Simple HTTP client\n"
+            "Usage: %% <URI>\n"
+            "Simple HTTP(S) client\n"
             "\n"
         ) << toolName;
     }
