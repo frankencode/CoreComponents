7d2eafbb1bffe222771b28ab6359a4a9e0950f91
Frank Mertens <frank@cyblogic.de>
2020-05-23 07:18:19 +0200

    Added another container type with value semantics: multimap

--- core/tests/test_multimap.cc
+++ core/tests/test_multimap.cc
@@ -1,21 +1,21 @@
 #include <cc/debug>
 #include <cc/stdio>
-#include <cc/map>
+#include <cc/multimap>
 
-namespace cc { template class map<int>; }
+namespace cc { template class multimap<int>; }
 
 int main(int argc, char *argv[])
 {
     using namespace cc;
 
-    map<int> a;
+    multimap<int> a;
     for (int i = 0; i < 10; ++i)
         a << Pair<int>{i, i * i};
 
     CC_INSPECT(a);
 
-    map<int> b = { Pair<int>{1, 1}, Pair<int>{2, 2}, Pair<int>{3, 4}, Pair<int>{4, 6} };
-    map<int> c = b;
+    multimap<int> b = { Pair<int>{1, 1}, Pair<int>{1, 1}, Pair<int>{2, 2}, Pair<int>{3, 4}, Pair<int>{4, 6} };
+    multimap<int> c = b;
 
     for (auto i = b->begin(); i; ++i) ++i->value();
     CC_INSPECT(b);
