419a586c4cff508858652ae751f91d4ffe0a0fa6
Frank Mertens <frank@cyblogic.de>
2020-06-28 03:21:37 +0200

    Fix: syntax error

--- crypto/src/RandomSource.cc
+++ crypto/src/RandomSource.cc
@@ -22,8 +22,8 @@ Ref<RandomSource> RandomSource::open(const CharArray *salt)
 
 RandomSource::RandomSource(const CharArray *salt)
 {
-    String key = String::allocate{16};
-    String iv = String::allocate{AesCipher::BlockSize};
+    String key = String::allocate(16);
+    String iv = String::allocate(AesCipher::BlockSize);
 
     if (salt) {
         mutate(key)->write(salt);
