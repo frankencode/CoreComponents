46bd5161427c55d0bb55a7a705a437a03dc365e6
Frank Mertens <frank@cyblogic.de>
2022-12-27 11:55:47 +0100

    Improve usability of BitArray

--- Core/tests/testBitArray.cc
+++ Core/tests/testBitArray.cc
@@ -8,12 +8,23 @@ int main(int argc, char *argv[])
     TestCase {
         "BitSetAndGet",
         []{
+            CC_CHECK(BitArray{}.count() == 0);
+
             BitArray bits { 23 };
             for (int i = 0; i < bits.count(); ++i) {
                 bits[i] = i & 1;
             }
             for (int i = 0; i < bits.count(); ++i) {
                 CC_VERIFY(bits[i] == (i & 1));
+            }
+
+            for (auto x: bits) x.toggle();
+
+            BitArray other = bits.copy();
+            other.fill(false);
+
+            for (int i = 0; i < bits.count(); ++i) {
+                CC_VERIFY(bits[i] != (i & 1));
             }
         }
     };
